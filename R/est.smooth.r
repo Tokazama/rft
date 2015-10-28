14166ef051dc724b38fdb244c2683a870712b8da
Tokazama <zchristensen7@gmail.com>
2015-10-28 12:46:13 -0600

    Update est.smooth.r

--- R/est.smooth.r
+++ R/est.smooth.r
@@ -3,54 +3,42 @@
 est.smooth<-function(mat,mask,psdf){
 	voxels<-ncol(mat)
 	subs<-length(mat)
-	Mmat<-matrix(nrow=1,ncol=voxels)
-	for (i in 1:voxels){
-		Mmat[1,i]<-colMeans(mat[,i])
-		}
-	Zmat<-(mat-Mmat)/S
+	Mmat<-colMeans(mat)
 	dimx<-dim(mask)[1]
 	dimy<-dim(mask)[2]
 	dimz<-dim(mask)[3]
 	for (i in 1:subs){
-		Zmat[,i]<-(mat[,i]-Mmat[,1])/(
+		Zmat[i,]<-(mat[i,]-Mmat)/psdf
 		}
-	Zxx<-0
-	Zyy<-0
-	Zzz<-0
-	Zxy<-0
-	Zyz<-0
-	Zxz<-0
+	lambda<-matrix(nrow=3,ncol=3)
 	for (i in 1:subs){
 		img<-makeImage(mask,Zmat[i,]
 		for (x in 1:(dimx)){
 			for (y in 1:(dimy)){
 				for (z in 1:(dimz)){
+					suppressWarnings(warning("index not inside the image : "[ x, y, z, ]))
 					vox<-getPixels(img,x,y,z)[1]
-					Zxx<-Zxx+(((getPixels(img,x+1,y,z)[1]-vox)^2)/(voxels*(subs-1)))
-					Zyy<-Zyy+(((getPixels(img,x,y+1,z)[1]-vox)^2)/(voxels*(subs-1)))
-					Zzz<-Zzz+(((getPixels(img,x,y,z+1)[1]-vox)^2)/(voxels*(subs-1)))
-					Zxy<-Zxy+(((vox+getPixels(img,x,y+1,z)[1])*(vox+getPixels(img,x+1,y,z)[1]))/(4*voxels*(subs-1)))
-					Zxz<-Zxz+(((vox+getPixels(img,x,y,z+1)[1])*(vox+getPixels(img,x+1,y,z)[1]))/(4*voxels*(subs-1)))
-					Zyz<-Zyz+(((vox+getPixels(img,x,y+1,z)[1])*(vox+getPixels(img,x,y,z+1)[1]))/(4*voxels*(subs-1)))		
+					lambda[1,1]<-lambda[1,1]+(((getPixels(img,x+1,y,z)[1]-vox)^2)/(voxels*(subs-1)))
+					lambda[2,2]<-lambda[2,2]+(((getPixels(img,x,y+1,z)[1]-vox)^2)/(voxels*(subs-1)))
+					lambda[3,3]<-lambda[3,3]+(((getPixels(img,x,y,z+1)[1]-vox)^2)/(voxels*(subs-1)))
+					lambda[1,2]<-lambda[1,2]+(((vox+getPixels(img,x,y+1,z)[1])*(vox+getPixels(img,x+1,y,z)[1]))/(4*voxels*(subs-1)))
+					lambda[1,3]<-lambda[1,3]+(((vox+getPixels(img,x,y,z+1)[1])*(vox+getPixels(img,x+1,y,z)[1]))/(4*voxels*(subs-1)))
+					lambda[3,2]<-lambda[3,2]+(((vox+getPixels(img,x,y+1,z)[1])*(vox+getPixels(img,x,y,z+1)[1]))/(4*voxels*(subs-1)))		
 					}
 				}
 			}
 		}
-	lambda<-dim(nrow=3,ncol=3)
-	lambda[1,1]<-(4*log(2/Zxx))^(1/2)
-	lambda[2,2]<-(4*log(2/Zyy))^(1/2)
-	lambda[3,3]<-(4*log(2/Zzz))^(1/2)
-	lambda[1,2]<-(4*log(2/Zxy))^(1/2)
-	lambda[2,1]<-(4*log(2/Zxy))^(1/2)
-	lambda[1,3]<-(4*log(2/Zxz))^(1/2)
-	lambda[3,1]<-(4*log(2/Zxz))^(1/2)
-	lambda[2,3]<-(4*log(2/Zyz))^(1/2)
-	lambda[3,2]<-(4*log(2/Zyz))^(1/2)
+	lambda[2,1]<-lambda[1,2]
+	lambda[3,1]<-lambda[1,3]
+	lambda[2,3]<-lambda[3,2]
+
+	fwhmx<-(4*log(2/lambda[1,1]))^(1/2)
+	fwhmy<-(4*log(2/lambda[2,2]))^(1/2)
+	fwhmz<-(4*log(2/lambda[3,3]))^(1/2)
 	fwhm<-c(fwhmx,fwhmy,fwhmz)
-	lambda<-dim(nrow=3,ncol=3)
 ##Tpdf<-probability density function of a t-distribution with v degress of freedom
 	vintegral<-function(t){((((t^2)+subs-1)^2)/((v-1)*(v-2)))*((dt(t,df)^3)/(p(t)^2))}
 	lambdav<-integrate(vintegral,Inf,Inf)
 	lambdaz<-lambdav*lambdae
-	return(fwhm)
+	return(fwhm,lambda)
 	}
