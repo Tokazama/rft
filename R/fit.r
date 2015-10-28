320c8700f3331b9d2d26195fe10ea88f72149e58
Tokazama <zchristensen7@gmail.com>
2015-10-28 09:09:22 -0600

    Update fit.r

--- R/fit.r
+++ R/fit.r
@@ -1,8 +1,23 @@
 library(ANTsR)
-setwd("/fslhome/zach8769/compute/rsobik/WASIMatrixReasoning/")
-load("/fslhome/zach8769/compute/rsobik/WASIMatrixReasoning/vardata")
-load("/fslhome/zach8769/compute/rsobik/WASIMatrixReasoning/varmat")
-mask<-getMask(antsImageRead("/fslhome/zach8769/compute/7_13_template/brainmask.nii.gz"))
+setwd("/fslhome/zach8769/compute/jacsobik/")
+ilist<-list.files()
+mask<-getMask(antsImageRead(ilist[1]))
+mat<-imagesToMatrix(ilist, mask)
+
+#Load data table
+sobik<-read.table("Desktop/data/sobik.csv", header=TRUE,sep=",")
+#Subset data to demographics and desired variable columns
+vardata<-sobik[c(1:9,12)]
+#Get rid of of rows missing data)
+vardata<-na.omit(vardata)
+#Create list of the rows remaining in the data table
+varlist<-rownames(vardata)
+varlist<-as.numeric(varlist)
+varlist<-list(varlist)
+#
+for(i in varlist){
+  varmat<-subset(mat[i,])
+  }
 #prepare contrasts
 ig<-vardata$InjuryGroup
 ig<-factor(ig,levels=c("oi","mild","mod","sev"))
