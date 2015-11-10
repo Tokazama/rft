60b41c40cbdfbf28dec18cbeb3841b6ae5a8b5b7
Tokazama <zchristensen7@gmail.com>
2015-11-10 13:59:29 -0700

    Update rft.pcluster.r

--- R/rft.pcluster.r
+++ R/rft.pcluster.r
@@ -1,14 +1,61 @@
+#' @name rft.pcluster
+#' @title Calculates probability of 
+#' A quick heuristic for thresholding a Statistical field using RFT
 #'
 #'
-#' calculates the cluster level p-value using RFT
+#' @param cMask-antsImage mask of the cluster
+#' @param bMask-antsImage mask of the brain
+#' @param fwhm-full width at half maxima
+#' @param stat-statistical value that was used to threshold the image
+#' @param df-degrees of freedom expressed as df[degrees of interest, degrees of error]
+#' @param fieldType:
+#'	"T"- T-field
+#'	"F"- F-field
+#'	"X"- Chi squar field
+#'	"Z"- Gaussian field
+#' @return Outputs a statistical value to be used for threshold a SPM
+#' @reference Friston K.J., (1994) Assessing the Significance of Focal Activations Using Their Spatial Extent.
+#' @reference Friston K.J., (1996) Detecting Activations in PET and fMRI: Levels of Inference and Power.
 #'
+#' @examples
 #'
+#'  var1<-vardata[,10]
+#'  subs<-nrow(varmat)
+#'  voxels<-ncol(varmat)
+#'  regpval<-matrix(nrow=1,ncol=voxels)
+#'  regtstat<-matrix(nrow=1,ncol=voxels)
+#'  resmat<-matrix(OL,nrow=subs,ncol=voxels)
+#'  for (i in 1:voxels){
+#'	  vox<-varmat[,i]
+#'	  regfit<-lm(vox~var1)
+#'	  ##Extract statistical values
+#'	  resmat[,i]<-residuals(regfit)
+#'	  regsum<-summary(regfit)
+#'	  regtstat[,i]<-regsum$coefficients[3,3]
+#'	  }
+#'  fwhm<-estPooled.smooth(res,rdf,mask)
+#'	negclust<-image2ClusterImages(timg,150,-Inf,-thresh)
+#'	negtable<-matrix(ncol=5)
+#'	colnames(postable)<-c("Voxels", "Cluster-Probability", "Peak-Height", "Voxel-Probability", "Coordinates")
+#'	for (i in 1:length(posclust)){
+#'	  cat("Determing negative cluster level statistics:",i,sep=" ")
+#'	  cmask<-getMask(negclust[[i]])
+#'	  cvoxs<-sum(as.array(cmask))
+#'	  pclust<-rft.pcluster(negclust[[i]],mask,fwhm,thresh,df,fieldType)
+#'	  peak<-max(posclust[[i]])
+#'	  loc<-labelImageCentroids(posclust[[i]])[2]
+#'	  resel <-ants.resel(mask,fwhm)
+#'	  ec<-ants.ec(stat,fieldType,df)
+#'	  pval<-(resel[1]*ec[1])+(resel[2]*ec[2])+(resel[3]*ec[3])+(resel[4]*ec[4])
+#'	  negtable[i,]<-c(cvox, pclust, peak, pval, loc$vertices[1],loc$vertices[2],loc$vertices[3])
+#'	  clustername<-paste("N-Cluster:",i,sep="")
+#'	  rownames(postable[i,])<-c(clustername)
+#'	  image<-paste(fileDir,"Nlcuster",i,".nii.gz",sep="")
+#'	  antsImageWrite(negclust[[i]],file=image)
+#'	  }
 #'
-#' 
-#' 
-#' 
-#'
-rft.pcluster<-function(cMask,bMask,fwhm,stat,df,fieldtype="T",D){
+#' @export rft.pcluster
+rft.pcluster<-function(cMask,bMask,fwhm,stat,df,fieldType){
 	voxels<-sum(as.array(mask))
 	D<-length(dim(mask))
 	if(class(cMask)=="numeric"){
