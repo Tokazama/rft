d21d3aaada3ee6ff3dfd38a0c8f2afc40a5458ed
Tokazama <zchristensen7@gmail.com>
2015-11-09 20:34:16 -0700

    Update rft.thresh.r

--- R/rft.thresh.r
+++ R/rft.thresh.r
@@ -1,17 +1,58 @@
+#' @name rft.thresh
+#' @title Calculates 
 #' A quick heuristic for thresholding a Statistical field using RFT
 #'
 #'
-#' @param pval-
-#' @param fwhm-
-#' @param mask-
-#' @param df-
-#' @param fieldtype-
-#' @return Outputs the estimated fwhm and covariance matrix that was used to estimate it
+#' @param pval-probability of false positive
+#' @param ka-minimum desired cluster size
+#' @param fwhm-full width at half maxima
+#' @param mask-antsImage mask
+#' @param df-degrees of freedom expressed as df[degrees of interest, degrees of error]
+#' @param fieldType:
+#'	"T"- T-field
+#'	"F"- F-field
+#'	"X"- Chi squar field
+#'	"Z"- Gaussian field
+#' @return Outputs a statistical value to be used for threshold a SPM
 #' @examples
-#'	
 #'
-#' @export est.smooth
-rft.thresh<-function(pval,ka,fwhm,mask,df,fieldtype){
+#'  var1<-vardata[,10]
+#'  subs<-nrow(varmat)
+#'  voxels<-ncol(varmat)
+#'  regpval<-matrix(nrow=1,ncol=voxels)
+#'  regtstat<-matrix(nrow=1,ncol=voxels)
+#'  resmat<-matrix(OL,nrow=subs,ncol=voxels)
+#'  for (i in 1:voxels){
+#'	  vox<-varmat[,i]
+#'	  regfit<-lm(vox~var1)
+#'	  ##Extract statistical values
+#'	  resmat[,i]<-residuals(regfit)
+#'	  regsum<-summary(regfit)
+#'	  regtstat[,i]<-regsum$coefficients[3,3]
+#'	  }
+#'  fwhm<-estPooled.smooth(res,rdf,mask)
+#'	negclust<-image2ClusterImages(timg,150,-Inf,-thresh)
+#'	negtable<-matrix(ncol=5)
+#'	colnames(postable)<-c("Voxels", "Cluster-Probability", "Peak-Height", "Voxel-Probability", "Coordinates")
+#'	for (i in 1:length(posclust)){
+#'	  cat("Determing negative cluster level statistics:",i,sep=" ")
+#'	  cmask<-getMask(negclust[[i]])
+#'	  cvoxs<-sum(as.array(cmask))
+#'	  pclust<-rft.pcluster(negclust[[i]],mask,fwhm,thresh,df,fieldType)
+#'	  peak<-max(posclust[[i]])
+#'	  loc<-labelImageCentroids(posclust[[i]])[2]
+#'	  resel <-ants.resel(mask,fwhm)
+#'	  ec<-ants.ec(stat,fieldType,df)
+#'	  pval<-(resel[1]*ec[1])+(resel[2]*ec[2])+(resel[3]*ec[3])+(resel[4]*ec[4])
+#'	  negtable[i,]<-c(cvox, pclust, peak, pval, loc$vertices[1],loc$vertices[2],loc$vertices[3])
+#'	  clustername<-paste("N-Cluster:",i,sep="")
+#'	  rownames(postable[i,])<-c(clustername)
+#'	  image<-paste(fileDir,"Nlcuster",i,".nii.gz",sep="")
+#'	  antsImageWrite(negclust[[i]],file=image)
+#'	  }
+#'
+#' @export rft.thresh
+rft.thresh<-function(pval,ka,fwhm,mask,df,fieldType){
 	voxels <-sum(as.array(mask))
 	bMask <-mask
 	cMask <- ka
@@ -22,7 +63,7 @@ rft.thresh<-function(pval,ka,fwhm,mask,df,fieldtype){
 	
 	while(alpha < pval){
 		stat <-stat-.01
-		alpha <-rft.cluster(cMask,bMask,fwhm,stat,df,fieldtype="T",D)
+		alpha <-rft.cluster(cMask,bMask,fwhm,stat,df,fieldType,D)
 		}
 	return(stat)
 	}
