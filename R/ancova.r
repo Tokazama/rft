f428095e0a9aea8500390af311b9670bf6ef9625
Tokazama <zchristensen7@gmail.com>
2015-11-11 15:54:54 -0700

    Update ancova.r

--- R/ancova.r
+++ R/ancova.r
@@ -50,24 +50,35 @@ contrasts(ig)=my.contrasts
 #Fit ancova model
 voxels<-ncol(varmat)
 subs<-nrow(varmat)
-tstat<-matrix(nrow=1, ncol=voxels)
 res<-matrix(nrow=subs, ncol=voxels)
+tstat.mild<-rep(1,ncol(varmat))
+tstat.mod<-rep(1,ncol(varmat))
+tstat.sev<-rep(1,ncol(varmat))
 
 progress <- txtProgressBar(min = 0, max = voxels, style = 3)
 for (i in 1:voxels){
   vox<-varmat[,i]
   fit<-lm(vox~var1+ig)
   res[,i]<-residuals(fit)
-  regsum<-summary(fit)
-  tstat[,i]<-regsum$coefficients[3,3]
+  sumfit<-summary(fit)
+	tstat.mild[i]<-sumfit$coefficients[3,3]
+	tstat.mod[i]<-sumfit$coefficients[4,3]
+	tstat.sev[i]<-sumfit$coefficients[5,3]
   setTxtProgressBar(progress, i)
   }
 close(progress)
 
+tmild<-makeImage(mask,tstat.mild)
+antsImageWrite(tmild,file='/Users/zach8769/Desktop/rsobik/BASCtAggression/tmild.nii.gz')
+tmod<-makeImage(mask,tstat.mod)
+antsImageWrite(tmod,file='/Users/zach8769/Desktop/rsobik/BASCtAggression/tmod.nii.gz')
+tsev<-makeImage(mask,tstat.sev)
+antsImageWrite(tsev,file='/Users/zach8769/Desktop/rsobik/BASCtAggression/tsev.nii.gz')
+
 rdf<-fit$df.residuals
 
-fwhm<-estPooled.smooth(res,rdf,mask)
+fwhm<-estScaled.smooth(res,rdf,mask)
 
 clusters<-rft.thresh(timg,.05,150,fwhm,mask,df,"T")
 
-voxels<-rft.voxel(cluster$stat,"T",df,fwhm,mask)
+voxels<-rft.voxel(cluster$stat,fwhm,mask,df,"T")
