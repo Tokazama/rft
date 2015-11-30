44334bf28cf661b4dca840377bd4b7ceda899223
Tokazama <zchristensen7@gmail.com>
2015-11-29 17:27:19 -0700

    Added degrees of freedom

--- R/model.rft.r
+++ R/model.rft.r
@@ -1,36 +1,60 @@
 
-model.rft <-function(variables,conditions,modelType){
-	ncon <-ncol(conditions)
-	nvar <-ncol(variables)
-	conlev <-1
+model.rft <-function(variables,conditions,controls,modelType){
 	if (modelType=="regression"){
 		if (class(variables) !="matrix" | class(variables) !="data.frame"){
-			variables <-as.matrix(variables)
+			variables <-as.data.frame(variables)
 			}
+		nvar <-ncol(variables)
 		formula <-~ variables - 1
 		dm <-model.matrix(formula)
 		dm <-cbind(dm,1)
+		DF <-nrow(dm)-ncol(dm)
+	#Creates design matrix based on conditions
 	}else if(modelType=="anova" | modelType=="ancova"){
-		if (class(conditions) !="matrix" | class(conditions) !="data.frame"){
-			conditions <-as.matrix(variables)
+		if (missing(conditions)){
+			stop("Both anova and ancova modelTypes must have conditions specified")
 			}
-		if (ncol(conditions) > 1){
+		if (class(conditions) !="matrix" | class(conditions) !="data.frame"){
+			conditions <-as.data.frame(variables)
+			}
+		ncon <-ncol(conditions)
+		if (ncon > 1){
 			formula <-~
 			for (n in 1:ncon){
-				formula <-formula*conditions[,n]
+				formula <-formula:conditions[,n]
 				}
 			dm <-model.matrix(formula-1)
 		}else{
 			dm <-model.matrix(~conditions-1)
 		}
+		nsub <-nrow(dim)
+		dmcol <-ncol(dm)
+		DF <-nsub-dmcol
 	}else{
 		stop("Must specify appropriate modelType")
 	}
+	#This creates the interaction between the each specified variable and condition
 	if (modelType=="ancova"){
-		dm <-cbind(variables,dm)
+		if (missing(variables)){
+			stop("Ancova modelType must have variables specificied")
+			}
+		nvar <-ncol(variables)
+		tmpmat <-matrix(1L,nrow=nsub,ncol=1)
+		for (i in 1:nvar){
+			var <-variables[,i]
+			varmat <-matrix(rep(var,dmcol),ncol=dmcol)
+			tmpmat <-cbind((dm *varmat),tmpmat)
+			}
 		}
-	dm <-cbind(dm,1)
-	colnames(dm)[ncol(dm)] <-"Intercept"
+	if (missing(controls)){
+		colnames(dm)[ncol(dm)] <-"Intercept"
+	}else {
+		if (class(controls) !="matrix" | class(controls) !="data.frame"){
+			controls <-as.data.frame(controls)
+			}
+		dm <-cbind(controls,dm)
+		colnames(dm)[ncol(dm)] <-"Intercept"
+	}
 	design.matrix <-list()
 	design.matrix <-lappend(design.matrix,dm)
 	design.matrix <-lappend(design.matrix,DF)
