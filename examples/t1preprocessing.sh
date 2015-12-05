259ca2e04a650e02c5427702da3527606dab7ff7
Tokazama <zchristensen7@gmail.com>
2015-12-04 21:57:17 -0700

    Update and rename t1processing.sh to t1preprocessing.sh

--- examples/t1preprocessing.sh
+++ examples/t1preprocessing.sh
@@ -36,9 +36,7 @@ mkdir $subjDir/atlas
 N4BiasFieldCorrection -d 3 -i ${array[$i]} -o ${t1}/n4.nii.gz -s 8 -b [200] -c [50x50x50x50,0.000001]
 N4BiasFieldCorrection -d 3 -i ${t1}/n4.nii.gz -o ${t1}/n4.nii.gz -s 4 -b [200] -c [50x50x50x50,0.000001]
 N4BiasFieldCorrection -d 3 -i ${t1}/n4.nii.gz -o ${t1}/n4.nii.gz -s 2 -b [200] -c [50x50x50x50,0.000001]
-
-#ResampleImage 3 ${t1}/n4.nii.gz ${t1}/n4_resliced.nii.gz 1x1x1
-/fslhome/zach8769/bin/c3d ${t1}/n4.nii.gz -resample-mm 1x1x1mm -o ${t1}/n4_resliced.nii.gz
+ResampleImage 3 ${t1}/n4.nii.gz ${t1}/n4_resliced.nii.gz 1x1x1
 
 antsBrainExtraction.sh -d 3 \
 -a ${t1}/n4_resliced.nii.gz \
