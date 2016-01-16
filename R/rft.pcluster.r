ac049dcf0238a960305c93da81bd0864f7148336
Tokazama <zchristensen7@gmail.com>
2016-01-15 18:26:43 -0700

    Update rft.pcluster.r

--- R/rft.pcluster.r
+++ R/rft.pcluster.r
@@ -5,20 +5,25 @@
 #' of code/{stat} used to extract said cluster. The degrees of freedom and statistical 
 #' field type used to obtain the original statistical map are also required. 
 #' 
-#' @param cMask - antsImage mask of the cluster
-#' @param bMask - antsImage mask of the brain
-#' @param fwhm - full width at half maxima
-#' @param stat - statistical value that was used to threshold the image
-#' @param df - degrees of freedom expressed as df[degrees of interest, degrees of error]
+#' @param u Threshold
+#' @param k Cluster size
+#' @param c Number of clusters
+#' @param mask 
+#' @param df degrees of freedom expressed as df[degrees of interest, degrees of error]
 #' @param fieldType:
-#'	"T" - T-field
-#'	"F" - F-field
-#'	"X" - Chi-square field
-#'	"Z" - Gaussian field
-#' @return Outputs a 
-#' @reference Friston K.J., (1994) Assessing the Significance of Focal Activations Using Their Spatial Extent.
-#' @reference Friston K.J., (1996) Detecting Activations in PET and fMRI: Levels of Inference and Power.
-#'
+#' \describe{
+#' \item{"T"}{T-field} 
+#' \item{"F"}{F-field} 
+#' \item{"X"}{Chi-square field"} 
+#' \item{"Z"}{Gaussian field}
+#' }
+#' 
+#' @return The probability of obtaining the specified cluster
+#' @reference 
+#' Friston K.J., (1994) Assessing the Significance of Focal Activations Using Their Spatial Extent.
+#' Friston K.J., (1996) Detecting Activations in PET and fMRI: Levels of Inference and Power.
+#' @Author Zachary P. Christensen
+#' @note: function currently in beta phase. Waiting for acceptance of peer-reviewed paper
 #' @examples
 #' 
 #' ## estimatation of a single images smoothness
@@ -32,41 +37,66 @@
 #' pval <-rft.pcluster(3, clustmask, clustimg, thresh, df, fwhm[[1]], fieldType="T")
 #'
 #' @export rft.pcluster
-rft.pcluster<-function(D,cMask,bMask,fwhm,stat,df,fieldType){
+rft.pcluster <-function(D, u, k, c, bMask, fwhm, stat, df, fieldType){
 	bvox<-sum(as.array(bMask))
-	if(class(cMask)=="numeric"){
-		ka<-cMask
-	}else{
-		ka<-sum(as.array(cMask))
-		}
 	fwhm<-mean(fwhm)
+	eps <-.Machine$double.eps
 	
 	if (fieldType=="T"){
-		EN<-bvox*(1-pt(stat,df))
+		EN <-bvox*(1-pt(stat,df))
 	}else if(fieldType=="F"){
-		EN<-bvox*(1-pf(stat, df[1],df[2]))
+		EN <-bvox*(1-pf(stat, df[1],df[2]))
 	}else if(fieldType=="X"){
-		EN<-bvox*(1-pchisq(stat, df[1],df[2]))
+		EN <-bvox*(1-pchisq(stat, df[1],df[2]))
 	}else if(fieldType=="Z"){
-		EN<-bvox*(1-qnorm(stat))
+		EN <-bvox*(1-qnorm(stat))
 	}else{
 		stop("A correct fieldtype is required")
 		}
+
 	#Based upon the fwhm values or the covariance matrix 
 	#of a fields partial derivatives(friston 1994)
-	W<-fwhm/((4*log(2))^(1/2))
+	W <-fwhm/((4*log(2))^(1/2))
 	
 	#The expected number of maxima/clusters (friston 1996; equation 2)
-	Em<-bvox*(2*pi)^(-(D+1)/2)*(W^(-D))*(stat^(D-1))*(exp((-stat^2)/2))
+	Em <-bvox*(2*pi)^(-(D+1)/2)*(W^(-D))*(stat^(D-1))*(exp((-stat^2)/2))
 	
 	#Used to derive the number of expected clusters in a 
 	#voxel (friston 1996;equation 3)
-	rfB<-(gamma((D/2)+1)*Em/EN)^(2/D)
+	rfB <-(gamma((D/2)+1)*Em/EN)^(2/D)
 	
 	#Equation 4 (friston 1996) The probability of getting a
 	#cluster of the size 'ka' or more above a threshold 'stat'
 	#for a single cluster
-	Pnk<-exp(-rfB*(ka^(2/D)))
-	alpha<-1-exp(-Em*Pnk)
-	return(alpha)
+	Pnk <-exp(-rfB*(ka^(2/D)))
+	
+	gamma(Em*Pnk)
+	
+	punc <-1-exp(-Em*Pnk)
+	ec <-rft.ec(stat,fieldType,df)
+	pstat <-(resel[1]*ec[1])+(resel[2]*ec[2])+(resel[3]*ec[3])+(resel[4]*ec[4])
+	pcor <-1-ppois(c-1,(pstat+eps)*punc)
+	z <-list(Pcor=pcor,
+		Punc=punc,
+		Ec=,
+		Ek=
 	}
+	
+mx.exp <- function (X, n) {
+	if (n != round(n)) {
+        n <- round(n)
+        warning("rounding exponent `n' to", n)
+    }
+    phi <- diag(nrow = nrow(X))
+    pot <- X
+    while (n > 0) {
+        if (n%%2) 
+            phi <- phi %*% pot
+        n <- n%/%2
+        pot <- pot %*% pot
+    }
+    return(phi)
+}
+	P <-toeplitz( as.numeric(t(EC) * G) ); P[lower.tri(P)] <- 0.0
+	P <-mx.exp(P, n)
+	P <-P[1,]
