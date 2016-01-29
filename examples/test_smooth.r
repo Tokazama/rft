b17a918a8fa281c094764707920c9fd0c38fd738
Tokazama <zchristensen7@gmail.com>
2016-01-29 13:19:49 -0700

    Create test_smooth.r

---
+++ examples/test_smooth.r
@@ -0,0 +1,45 @@
+root <-"/Users/zach8769/Desktop/"
+ptbp <-read.csv(paste(root,"/ptbp/data/ptbp_summary_demographics.csv",sep=""),header=TRUE,sep=",")
+ptbp <-ptbp[c(1:5)]
+ptbp <-na.omit(ptbp)
+rows.ptbp.train <-sample(nrow(ptbp), round(.5*nrow(ptbp)), replace=F)
+ptbp.train <-ptbp[rows.ptbp.train,]
+age<-ptbp.train$AgeAtScan
+mask <-antsImageRead(paste(root,"/ptbp/ptbp_template/PTBP_T1_BrainCerebellumMask.nii.gz",sep=""))
+sub <-ptbp.train[,1]
+date <-ptbp.train[,2]
+paths.ptbp.train <-paste(root,"ptbp/rft/warp/", sub, "_", date, ".nii.gz",sep="")
+img.list <-list()
+x <-cbind(1L,age)
+conmat <-matrix(c(0,1),nrow=1)
+colnames(x) <-c("Intercept","Age")
+colnames(conmat) <-c("Intercept","Age")
+
+for (i in paths.ptbp.train){
+	img.list[i] <-smoothImage(antsImageRead(i), 1)
+	}
+imat1 <-imageListToMatrix(img.list, mask)
+fit1 <-rft.lm(x,imat1,conmat,mask)
+fwhm1 <-fit1$fwhm
+# fwhm=c(4.755874,4.439168,4.309534)
+# resels=c(1 107.3829,2496.4898,14133.7936)
+
+
+for (i in paths.ptbp.train){
+	img.list[i] <-smoothImage(antsImageRead(i), 1.5)
+	}
+imat2 <-imageListToMatrix(img.list, mask)
+fit2 <-rft.lm(x,imat2,conmat,mask)
+fwhm2 <-fit2$fwhm
+
+# fwhm=c(4.841590,4.543965,4.438124)
+# resels=c(1,104.909.,2381.9027,13170.3890)
+
+for (i in paths.ptbp.train){
+	img.list[i] <-smoothImage(antsImageRead(i), 2)
+	}
+imat3 <-imageListToMatrix(img.list, mask)
+fit3 <-rft.lm(x,imat3,conmat,mask)
+
+# fwhm=c(4.977359, 4.677361, 4.586128)
+# resels=c(1.0000,   101.8433,  2244.1573, 12044.1197)
