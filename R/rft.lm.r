63d01f13e280cbfc35a3aa576ffe0436737013ab
Tokazama <zchristensen7@gmail.com>
2016-01-31 22:44:39 -0700

    more details in description

--- R/rft.lm.r
+++ R/rft.lm.r
@@ -2,28 +2,46 @@
 #' 
 #' @param x matrix or data frame of predictors
 #' @param y observations (i.e. an image matrix)
-#' @param conmat contrast matrix of contrasts * conditions (see example for more details)
+#' @param conmat contrast matrix of contrasts by conditions (see example for more details)
 #' @param mask object of type \code{antsImage}. Typically the same mask used to produce the image matrix.
 #' @param tol
 #' @param statdir directory that statistical images are saved to (defaualt to "./")
-#' @param findSmooth
-#' @param concise
-#' @param resSample
+#' @param findSmooth if \cod{findSmooth="FALSE"} then FWHM and resels aren't calculated (default is \code{"TRUE"}
+#' @param concise if \code{concise="FALSE"} then residuals and coefficients are returned (default is \code{"TRUE"})
+#' @param resSample how many of the residual images will be used to estimate FWHM and resels (default is all images)
 #'
-#' @return a list of statistical images, coefficients, the FWHM estimate, and resel values.
+#' @return A list comprising:
+#' \describe{
+#' \item{conmat}{contrast matrix used to create \code{Timgs}}
+#' \item{Designmatrix}{the design matrix used to fit the model}
+#' \item{df}{degrees of freedom}
+#' \item{residuals}{image matrix of residuals}
+#' \item{coefficients}{image matrix of coefficients}
+#' \item{Timgs}{list of the t-field images } 
+#' \item{fwhm}{the estimated FWHM}
+#' \item{RPVImg}{resels per voxel image}
+#' \item{resels}{estimated resels for the search space}
+#' }
 #' @description
+#'
 #'
 #' This function is a wrapper for several functions necessary to performing a random field theory based
 #' statistical analysis of images. A matrix ,or data frame, of predictors (\code{x}) is fitted to an 
-#' image matrix (\code{y}) using a general linear model. The intercept is supressed and set to x=0. 
-#' Therefore, \code{y} is scaled to zero. As recommended by ### et al. (1999) the residuals are used to 
-#' estimate the full width at half-maxima (FWHM; or "smoothness") of the final statistical image. The 
-#' the resolutions in voxel space (resels) are calculated using the estimated FWHM and the image search 
+#' image matrix (\code{y}) using \code{.lm.fit}. The images are each mean scaled to zero prior to
+#' fitting (Friston K.J., 1995). As recommended by Stefan et al. (1999), the residuals are used to 
+#' estimate the full width at half-maxima (FWHM; or "smoothness") of the final statistical image. The
+#' residuals are standardized using the mean sum of squares of the residuals.
+#' 
+#' Standardized Residuals = residuals/sqrt(MRSS/Error of Degrees of Freedom)
+#' 
+#' The the resolutions in voxel space (resels) are calculated using the estimated FWHM and the image search 
 #' region (\code{mask}). The \code{T.imgs} output contains a list of t-statistical fields in image space
-#' determined according to specified contrasts (\code{conmat}). 
+#' determined according to specified contrasts (\code{conmat}).
 #' 
 #'
+#'
 #' @References 
+#' Friston K.J., (1995) Statistical Parametric Maps in Functional Imaging: A General Linear Approach 
 #' Worsley K.J., (1992) A Three-Dimensional Statistical Analysis for CBF Activation Studies in Human Brain.
 #' Worlsey K.J., et al. (1996) A Unified Statistical Approach for Determining Significant Signals in Images of Cerebral Activation.
 #' Stefan J.K., (1999) Robust Smoothness Estimation in Statistical Parametric Maps Using Standardized Residual from the General Linear Model
