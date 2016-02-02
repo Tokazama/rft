e8e92287f7fc38b92657743053d29353c6d63c57
Tokazama <zchristensen7@gmail.com>
2016-02-02 15:07:23 -0700

    Update rft.ACF.r

--- R/rft.ACF.r
+++ R/rft.ACF.r
@@ -6,6 +6,9 @@
 #' rft.ACF.r
 rft.ACF <-function(){
 
+# 1. estimate beta using ordinary least-squares (OLS)
+# 2. Compute F ratio
+
 # y=X*beta+error
 # In= the identity matrix of rank 'n'
 # Xo denotes the subspace of the full model X
@@ -34,6 +37,23 @@ f = (yT*My)/(sd*tr(M))=((yT*My)/(tr(M)))/(yT*Ry/tr(R))
 
 Rml = In - X*((XT*(C^-1)*X)^-1)*XT*(C^-1)
 sd=(yT*RTml*(Q^-1)*Rml*y)/tr(Rml)
+
+# nonspherical variance component
+## for general linear model with single variance parameter and a nonspherical variance component Q F statistic and df can be estimated
+f = ((yT*My)/(tr(MQ))/((yT*Ry)/tr(RQ))
+vo = (tr(MQ)^2)/tr(MQMQ)
+v = (tr(RQ)^2)/tr(RQRQ)
+# variance parameter estimate
+sd = (yT*Ry)/tr(RQ)
+# <> denotes expectation and Var() the variance of a random variable (<sd>=expected sd)
+Var(<sd>) = (2*sd*tr(RQRQ))/(tr(RQ)^2)
+
+# Normal approximation
+## Taylor series expansion for log-likelihood log (p(y|lambdaEst))
+
+p(y|lambda) = N(lambdaEst, I(lambdaEst)^-1) 
+# I(lambdaEst) is the observed information matrix with:
+I(lambdaEst) = - (partial derivative lambda)*log(p(y|lambdaEst))
 
 
 }
