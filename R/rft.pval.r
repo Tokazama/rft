89a320602a069978f6c5f521433bb635bbe65e99
Tokazama <zchristensen7@gmail.com>
2016-02-13 09:09:46 -0700

    Update rft.pval.r

--- R/rft.pval.r
+++ R/rft.pval.r
@@ -6,14 +6,15 @@
 #' field type used to obtain the original statistical map are also required. 
 #' 
 #' set-level
-#' rft.pval(c, k, u, resels, df, fieldType)
+#' rft.pval(D, c, k, u, resels, df, fieldType)
 #' 
 #' cluster-level
-#' rft.pval(1, k, u, resels, df, fieldType)
+#' rft.pval(D, 1, k, u, resels, df, fieldType)
 #' 
 #' voxel-level
-#' rft.pval(1, 1, u, resels, df, fieldType)
+#' rft.pval(D, 1, 0, u, resels, df, fieldType)
 #' 
+#' @param D image dimensions
 #' @param c Threshold
 #' @param k Cluster size in resel space
 #' @param u Number of clusters
@@ -25,6 +26,7 @@
 #' \item{"X"}{Chi-square field"} 
 #' \item{"Z"}{Gaussian field}
 #' }
+#' @param n number of statistical fields in conjunction
 #' 
 #' @return The probability of obtaining the specified cluster
 #' Pcor corrected p-value
@@ -39,18 +41,10 @@
 #' @note: function currently in beta phase. Waiting for acceptance of peer-reviewed paper
 #' @examples
 #' 
-#' ## estimatation of a single images smoothness
-#' outimg1 <-makeImage(c(10,10,10), rnorm(1000))
-#' maskimg <-getMask(outimg1)
-#' fwhm <-est.Smooth(outimg1,maskimg)
-#' ## create arbitrary threshold and degrees of freedom from hypothetical analysis
-#' thresh <-mean(outimg1)
-#' df <-4
-#' clustimg <-thresholdImage(outimg1, thresh, Inf, inval=1, outval=0)
-#' pval <-rft.pcluster(3, clustmask, clustimg, thresh, df, fwhm[[1]], fieldType="T")
-#'
+#' 
+#' 
 #' @export rft.pval
-rft.pval <-function(D, c, k, u, resels, df, fieldType){
+rft.pval <-function(D, c, k, u, resels, df, fieldType, n=1){
   if(missing(fieldType)){
     stop("Must specify fieldType")
   }else if(missing(df)){
@@ -60,106 +54,54 @@ rft.pval <-function(D, c, k, u, resels, df, fieldType){
   }else if(missing(u) && missing(k) && missing(c)){
     stop("Must atleast specify one of u, k, or c")
   }
-
-  n <-1
-  
   G <-sqrt(pi)/gamma((1:(D+1)/2))
-  euler <-rft.ec(u, fieldType, df[2])
-  euler <-pmax(euler[1:(D+1)],.Machine$double.eps)
-  P <-toeplitz(as.numeric(euler*G)) 
+  ec <-c(0,0,0,0)
+  if (fieldType=="T"){
+    ec[1] <-1-pt(u,df[2])
+    ec[2] <-(((4*log(2))^(1/2))/(2*pi))*((1+((u^2)/df[2]))^(-1/2*(df[2]-1)))
+    ec[3] <-((((4*log(2))*lgamma((df[2]+1)))/(((2*pi)^(3/2))*((df[2]/2)^(1/2))*lgamma(df[2]/2)))*((1+((u^2)/2))^(-1/2*(df[2]-1))))*u
+    ec[4] <-(((4*log(2))^(3/2))/((2*pi)^2))*((1+((u^2)/df[2]))^(-1/2*(df[2]-1)))*((((df[2]-1)/df[2])*(u^2))-1)
+  }else if(fieldType=="F"){
+    ec[1] <-1-pf(u,df[1],df[2])
+    ec[2] <-(((4*log(2))^(1/2))/((2*pi)^(1/2)))*(((lgamma(df+k-1)/2)*2^(1/2))/(lgamma(df/2)*lgamma(k/2)))*((k*u/df)^(1/2(k-2)))*((1+(k*u/v))^(-1/2*(df+k-2)))
+    ec[3] <-((4*log(2))/(2*pi))*((lgamma((v+k-2)))/(lgamma(df/2)*lgamma(k/2)))*((k*u/df)^(1/2*(k-2)))*(1+(k*t/df))^(-1/2(df+k-2))*((v-1)*(k*u/v)-(k-1))
+    ec[4] <-(((4*log(2))^(3/2))/((2*pi)^(3/2)))*(((lgamma((v+k-2)))*2^(-1/2))/(lgamma(df/2)*lgamma(k/2)))*((k*t/df)^(1/2*(k-3)))*((1+(k*u/df))^(-1/2*(df+k-2)))*((df-1)*(df-2)*((k*u/v)^(2))-(2*df*k-df-k-1)*(k*u/df)+(k-1)*(k-2))
+  }else if(fieldType=="X"){
+    ec[1] <-1-pchisq(u,df[2])
+    ec[2] <-(((4*log(2))^(1/2))/(2*pi))*(((u^(1/2*(df-1)))*(e^(-1/2*u)))/((2^(1/2(df-2)))*gamma(df/2)))
+    ec[3] <-((4*log(2))/(2*pi))*(((u^(1/2*(df-2)))*(e^(-1/2*u)))/((2^(1/2*(df-2)))*gamma(df/2)))*(u-(v-1))
+    ec[4] <-((4*log(2)^(3/2))/((2*pi)^(3/2)))*(((u^(1/2*(df-3)))*(e^(-1/2*u)))/((2^(1/2*(df-2)))*(gamma(df/2))))*((u^2)-((2*df-1)*u)+(df-1)*(df-2))
+  }else if(fieldType=="Z"){
+    ec[1] <-1-pnorm(u,df[2])
+    ec[2] <-((((4*log(2))^(1/2))/(2*pi))*e^(-1/2*((u^2))))
+    ec[3] <-((4*log(2))/(2*pi))*u*(e^(-1/2*(u^2)))
+    ec[4] <-(((4*log(2))^(3/2))/((2*pi)^2))*((u^2)-1)*(e^(-1/2*(u^2)))
+  }
+  ec <-pmax(ec[1:(D+1)],.Machine$double.eps)
+  P <-toeplitz(as.numeric(ec*G))
   P[lower.tri(P)] <-0
-  # P <-mx.exp(P, n)
+  if (n !=round(n)){
+    n <-round(n)
+    warning("rounding exponent `n' to", n)
+  }
+  P <-diag(nrow = nrow(P))
+  pot <-P
+  while (n > 0){
+    if (n %% 2)(P <-P %*% pot)
+    n <-n %/% 2
+    pot <-pot %*% pot
+  }
   P <-P[1,]
   EM <-(resels[1:(D+1)]/G)*P # maxima in all dimensions
   Ec <-sum(EM) # number of overall expected maxima/clusters
-  EN <-P[1]*resels[D+1] # number of resels
+  EN <-P[1]*resels[D+1] # number of resels in entire image
   ek <-EN/EM[D+1] # expected number of resels per cluster
   
   rfB <-(gamma((D/2)+1)*ek)^(2/D)
-  Pu <-exp(-rfB*(k^(2/D)))
+  Pu <-exp(-rfB*(k^(2/D))) # cumulative cluster-size distribution from which uncorrected P values are calculated
   
-  if (c > 1){
-    # set-level
-    Pcor <-1-ppois(c-1,lambda=(Ec*Pu))
-  }else if (c==1 && k > 1){
-    # cluster-level
-    Pcor <-1-exp(-Ec*Pu)
-  }else if (c==1 && k==1){
-    # voxel-level
-    Pcor <-1-exp(-Ec)
-  }
+  Pcor <-1-ppois(c-1,lambda=(Ec+.Machine$double.eps)*Pu)
+  
   z <-list(Pcor=Pcor, Pu=Pu, Ec=Ec, ek=ek)
-  z
-}
-
-#' Calculates the euler characteristic
-#'
-#' Used in conjuction with \code{rft.resel} the voxel-level probability of a
-#' single voxel value can be determined. The voxel value is typically the peak 
-#' voxel of a cluster after thresholding a statistical image. 
-#' 
-#' 
-#' @param u statistical value (typically the maxima of a cluster or statistical map)
-#' @param fieldType:
-#' \describe{
-#' \item{"T"}{T-field} 
-#' \item{"F"}{F-field} 
-#' \item{"X"}{Chi-square field"} 
-#' \item{"Z"}{Gaussian field}
-#' }
-#' @param df degrees of freedom expressed as df[degrees of interest, degrees of error]
-#'
-#' @return ec a vector of estimated euler characteristics
-#'
-#' @References Worlsey K.J., et al. (1996) A Unified Statistical Approach for Determining Significant Signals in Images of Cerebral Activation.
-#' @Author Zachary P. Christensen
-#' @note: function currently in beta phase. Waiting for acceptance of peer-reviewed paper
-#' @examples
-#' 
-#' outimg1 <-makeImage(c(10,10,10), rnorm(1000))
-#' maskimg <-getMask(outimg1)
-#' fwhm <-est.Smooth(outimg1, maskimg)
-#' resel <-rft.resels(maskimg, fwhm[[1]])
-#' ec <-rft.ec(max(outimg1), fieldType="T", rnorm(1))
-#' pvox<-(resel[1]*ec[1])+(resel[2]*ec[2])+(resel[3]*ec[3])+(resel[4]*ec[4])
-#' 
-#' 
-#' @export rft.ec
-rft.ec<-function(u,fieldType,df){
-  ec<-c(0,0,0,0)
-  t<-u
-  if(fieldType=="T"){
-    a<-gamma((df[2]+1)/2)/(((df[2]*pi)^(1/2))*gamma(df[2]/2))
-    tintegral<-function(u){a*((1+(u^2)/df[2])^(-1/2*(df[2]+1)))}
-    iec<-integrate(tintegral,lower=u,upper=Inf)
-    ec[1]<-as.numeric(iec[1])
-    ec[2]<-(((4*log(2))^(1/2))/(2*pi))*((1+((u^2)/df[2]))^(-1/2*(df[2]-1)))
-    ec[3]<-((((4*log(2))*lgamma((df[2]+1)))/(((2*pi)^(3/2))*((df[2]/2)^(1/2))*lgamma(df[2]/2)))*((1+((u^2)/2))^(-1/2*(df[2]-1))))*u
-    ec[4]<-(((4*log(2))^(3/2))/((2*pi)^2))*((1+((u^2)/df[2]))^(-1/2*(df[2]-1)))*((((df[2]-1)/df[2])*(u^2))-1)
-  }else if(fieldType=="F"){
-    a<-(gamma((df+k-2)/2)/(gamma(df/2)*gamma(k/2)))
-    fintegral<-function(u){a*(((k*u/df)^(1/2*(k-2)))*(1+(k*u/2)^(-1/2*(df+k))))}
-    iec<-integrate(fintegral,lower=u,upper=Inf)
-    ec[1]<-as.numeric(iec[1])
-    ec[2]<-(((4*log(2))^(1/2))/((2*pi)^(1/2)))*(((lgamma(df+k-1)/2)*2^(1/2))/(lgamma(df/2)*lgamma(k/2)))*((k*u/df)^(1/2(k-2)))*((1+(k*u/v))^(-1/2*(df+k-2)))
-    ec[3]<-((4*log(2))/(2*pi))*((lgamma((v+k-2)))/(lgamma(df/2)*lgamma(k/2)))*((k*u/df)^(1/2*(k-2)))*(1+(k*t/df))^(-1/2(df+k-2))*((v-1)*(k*u/v)-(k-1))
-    ec[4]<-(((4*log(2))^(3/2))/((2*pi)^(3/2)))*(((lgamma((v+k-2)))*2^(-1/2))/(lgamma(df/2)*lgamma(k/2)))*((k*t/df)^(1/2*(k-3)))*((1+(k*u/df))^(-1/2*(df+k-2)))*((df-1)*(df-2)*((k*u/v)^(2))-(2*df*k-df-k-1)*(k*u/df)+(k-1)*(k-2))
-  }else if(fieldType=="X"){
-    xintegral<-function(u){((u^(1/2))*e^(-1/2*u))/((2^(df/2))*(gamma(df/2)))}
-    iec<-integrate(xintegral,lower=u,upper=Inf)
-    ec[1]<-as.numeric(iec[1])
-    ec[2]<-(((4*log(2))^(1/2))/(2*pi))*(((u^(1/2*(df-1)))*(e^(-1/2*u)))/((2^(1/2(df-2)))*gamma(df/2)))
-    ec[3]<-((4*log(2))/(2*pi))*(((u^(1/2*(df-2)))*(e^(-1/2*u)))/((2^(1/2*(df-2)))*gamma(df/2)))*(u-(v-1))
-    ec[4]<-((4*log(2)^(3/2))/((2*pi)^(3/2)))*(((u^(1/2*(df-3)))*(e^(-1/2*u)))/((2^(1/2*(df-2)))*(gamma(df/2))))*((u^2)-((2*df-1)*u)+(df-1)*(df-2))
-  }else if(fieldType=="G"){  
-    gintegral<-function(u){((1/((2*pi)^(1/2)))*e^(-1/2*(u^2)))}
-    iec<-integrate(gintegral,lower=u,upper=Inf)
-    ec[1]<-as.numeric(iec[1])
-    ec[2]<-((((4*log(2))^(1/2))/(2*pi))*e^(-1/2*((u^2))))
-    ec[3]<-((4*log(2))/(2*pi))*u*(e^(-1/2*(u^2)))
-    ec[4]<-(((4*log(2))^(3/2))/((2*pi)^2))*((u^2)-1)*(e^(-1/2*(u^2)))
-  }else{
-    stop("Incorrect input for FieldType")
-  }
-  return(ec)
+  return(z)
 }
