874b4d83bde131e753daa109585a08a9f27d9cb9
Tokazama <zchristensen7@gmail.com>
2016-02-27 21:29:35 -0700

    Update rft.fit.r

--- R/rft.fit.r
+++ R/rft.fit.r
@@ -1,75 +1,137 @@
-#' Fits model uing RFT contrasts
+#' Fits a linear model according to random field theory
 #' 
+#' @param formula
+#' @param conmat contrast matrix of contrasts by conditions (see example for more details)
+#' @param mask object of type \code{antsImage}. Typically the same mask used to produce the image matrix.
+#' @param tol (default = 1e-07)
+#' @param statdir directory that statistical images are saved to (if not specified results are not saved)
+#' @param sample how many of the residual images will be used to estimate FWHM and resels (default is all images)
+#' @param findSmooth if \cod{findSmooth="FALSE"} then FWHM and resels aren't calculated (default is \code{"TRUE"}
+#' @param concise if \code{concise="FALSE"} then residuals and coefficients are returned (default is \code{"TRUE"})
+#' @param Intercept include intercept in design model
+#' @param subMean scales each subject by image mean intensity (also uses a rough scale to physiologically meaningful values in PET (50 mls of blood / 100 mls of brain tissue /min))
+#' @param regMean creates a control variable for each subjects mean image intensity value
+#' @param groupMean creates a control variable for the mean image intensity value of each level of the identified factor 
+#' @param grandMean scales the entire by the mean of all images
+#' @param GG perform Greenhouse-Geisser correction for degrees of freedom?
+#' @param verbose
+#'
+#' @return A list comprising:
+#' \itemize{
+#' \item{conmat}{contrast matrix used to create \code{Timgs}}
+#' \item{Designmatrix}{the design matrix used to fit the model}
+#' \item{df}{degrees of freedom}
+#' \item{residuals}{image matrix of residuals}
+#' \item{coefficients}{image matrix of coefficients}
+#' \item{Timgs}{list of the t-field images } 
+#' \item{fwhm}{the estimated FWHM}
+#' \item{RPVImg}{resels per voxel image}
+#' \item{resels}{estimated resels for the search space}
+#' }
+#' @description
+#'
+#' The images are each mean scaled to zero prior to
+#' fitting (Friston K.J., 1995). As recommended by Stefan et al. (1999), the residuals are used to 
+#' estimate the full width at half-maxima (FWHM; or "smoothness") of the final statistical image. The
+#' residuals are standardized using the mean sum of squares of the residuals.
 #' 
+#' Standardized Residuals = residuals/sqrt(MRSS/Error of Degrees of Freedom)
 #' 
-#' @param X design matrix
-#' @param Y image matrix
-#' @param conmat matrix of contrasts
-#' @param conType a list of specifying the type of statistical field to be computed (i.e. t-values="T" or F-values="F")
-#'               corresponding to each row of conmat specifying of
-#' @param statdir directory to save contrast images
+#' The the resolutions in voxel space (resels) are calculated using the estimated FWHM and the image search 
+#' region (mask). The StatImgs output contains a list of t-statistical fields in image space
+#' determined according to specified contrasts (\code{conmat}).
+#' 
 #'
 #'
-#' @export rft.fit
-rft.fit <-function(X, Y, conmat, conType, statdir){
-  if (require("Matrix")){
-    cat("Using sparse matrices for analysis. \n")
-  }else {
-    stop("Matrix package is required for this function. \n")
-  }
-  if (ncol(conmat) !=ncol(X)){
-    stop("Object conmat must have the same number of columns as factors/variables included in the model.")
-  }
-  n <-nrow(X) # number of subjects
-
-  z <-.lm.fit(X,Y,tol=tol)
-  #  # TEST THIS: may not be worth the initial computational time investment
-  #  z$qr <-Matrix(z$qr, sparse=TRUE)
-  #  z$coefficients <-Matrix(z$coefficients, sparse=TRUE)
-  #  z$residuals <-Matrix(z$residuals, sparse=TRUE)
-  #  z$effects <-Matrix(z$effects, sparse=TRUE)
-  #  
-  #  
+#' @References 
+#' Friston K.J., (1995) Statistical Parametric Maps in Functional Imaging: A General Linear Approach 
+#' Worsley K.J., (1992) A Three-Dimensional Statistical Analysis for CBF Activation Studies in Human Brain.
+#' Worlsey K.J., et al. (1996) A Unified Statistical Approach for Determining Significant Signals in Images of Cerebral Activation.
+#' Stefan J.K., (1999) Robust Smoothness Estimation in Statistical Parametric Maps Using Standardized Residual from the General Linear Model
+#' 
+#' @Author Zachary P. Christensen
+#' @kewords rft.pval, rft.euler, rft.resels
+#' @note: function currently in beta phase. Waiting for acceptance of peer-reviewed paper
+#' @examples
+#' 
+#'
+#' @export rft.lm
+rft.lm <-function(formula, conmat, conType, mask, 
+                  tol=1e-07, statdir=NULL, sample=NULL, 
+                  intercept=TRUE, subMean=TRUE, regMean=FALSE, 
+                  groupMean=NULL, findSmooth=TRUE, concise=TRUE, 
+                  grandMean=FALSE, verbose=TRUE){
   
+  Y <-model.frame(formula)[[1L]]
+  nsub <-nrow(Y)
+  nvox <-sum(as.array(mask))
+  #myresp <-as.character(formula(formula)[2])
   
-  rss <-colSums(z$residuals^2)
-  Ip <-Diagonal(z$rank)
-  In <-Diagonal(n)
-  XX <-chol2inv(qr.R(x$qr))
-  
-  # df[degrees of interest, degrees of error]
-  df <-c(z$rank-1,n-z$rank)
-
-  # residual projecting matrix
-  R <-In-X %*% ginvSparse(X)
-
-  # retain value for computing F contrasts
-  YRY <-t(Y) %*% R %*% Y
-
-  qr <-z[c("qr", "qraux", "pivot", "tol", "rank")]
-  # Q <-qr.Q(qr)
-  
-  StatImgs <-list()
-  for (i in 1:nrow(conmat)){
-    if (conType[i]=="T"){
-      se <-t(as.matrix(sqrt((rss/df[2])*(conmat[i,] %*% XX %*% conmat[i,]))))
-      se[se==0] <-.01 # control for NULLS that result from dividing by 0
-      statmat <-(conmat[i,] %*% z$coefficients)/se
-    }else if(conType[i]=="F"){
-      c <-matrix(conmat[i,],ncol=1)
-      X0 <-X %*% (Ip-c %*% ginvSparse(c))
-      R0 <-In-X0 %*% ginvSparse(X0) # contrast projecting matrix
-      M <-R0-R
-      statmat <-((t(z$coefficients) %*% t(X) %*% M %*% z$coefficients)/z$YRY)*(df[1]/z$df[2])
+  if (subMean=="TRUE" | regMean=="TRUE" | !is.null(groupMean)){
+    g <-rowSums(Y)/nvox
+    
+    if (subMean=="TRUE"){
+      X <-model.matrix(update(formula, ~ . - 1))
+      for (sub in 1:nsub){
+        Y[nsub,] <-Y[nsub,]*(50/g[nsub])
+        X[nsub,] <-X[nsub,]*(g[nsub]/50)
+      }
     }
-    statimg <-makeImage(mask, statmat)
-    if (!missing(statdir)){
-      antsImageWrite(statimg, file=paste(statdir, rownames(conmat)[i], ".nii.gz", sep=""))
+    if(regMean=="TRUE"){
+      X <-model.matrix(update(formula, ~ . + g - 1))
+      conmat <-cbind(conmat,0)
     }
-    StatImgs <-lappend(StatImgs,statimg)
-    names(StatImgs)[length(StatImgs)] <-rownames(conmat)[i]
+    if(!is.null(groupMean)){
+      gg <-ave(g, groupMean)
+      X <-model.matrix(update(formula, ~ . + gg - 1))
+      conmat <-cbind(conmat,0)
+    }
+  }else{
+    X <-model.matrix(update(formula, ~ . - 1))
   }
-  if (!is.null(rownames(conmat)))
-    names(StatImgs) <-rownames(conmat)
-  c(z[c("coefficients", "residuals", "effects")], list(qr = structure(qr, class = "qr"), df=df, StatImgs=StatImgs))
+  # Grand Mean Scaling
+  if (grandMean=="TRUE")
+    Y <-Y * mean(Y[Y !=0])
+  
+  if (intercept=="TRUE"){
+    X <-cbind(X, 1L)
+    colnames(X)[ncol(X)] <-"Intercept"
+    conmat <-cbind(conmat,0)
+    colnames(conmat)[ncol(conmat)] <-"Intercept"
   }
+  
+  #### Fit General Linear Model ####
+  # fit using OLS and calculate contrasts
+  z <-rft.fit(X, Y, conmat, conType, statdir=statdir)
+  
+  #### Estimate FWHM/RESELS ####
+  if (findSmooth=="TRUE"){
+    
+    if (verbose)
+      cat("Estimating FWHM. \n")
+    
+    sr <-z$residuals/sqrt(z$rss/z$df[2])
+    fwhm <-estSmooth(sr, mask, df, sample=sample)
+    ans <-lappend(ans,fwhm$fwhm)
+    names(ans)[length(ans)] <-"fwhm"
+    ans <-lappend(ans,fwhm$RPVImg)
+    names(ans)[length(ans)] <-"RPVImg"
+    
+    if (verbose)
+      cat("Calculating resels. \n")
+    
+    resels <-rft.resels(mask, fwhm$fwhm)
+    ans <-lappend(ans,resels)
+    names(ans)[length(ans)] <-"resels"
+  }
+  
+  #### Prepare Output ####
+  ans <-list(X, resels, fwhm$fwhm, fwhm$RPVImg, z$coefficients, z$qr, z$df, z$StatImgs)
+  names(ans) <-c("design","resels","fwhm","RPVImg","coefficients","qr","df","StatImgs")
+  
+  if (concise=="FALSE"){
+    ans <-lappend(ans, z$residuals)
+    names(ans)[length(ans)] <-"residuals"
+  }
+  ans
+}
