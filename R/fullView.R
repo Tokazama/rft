6087f32711c3bdb07bddff1597a3bfcc381ee945
Tokazama <zchristensen7@gmail.com>
2016-03-25 17:11:59 -0600

    Update fullView.R

--- R/fullView.R
+++ R/fullView.R
@@ -1,11 +1,34 @@
 #'
+#' @param axis the axis to slice (1 , 2 or 3)
+#' @param slice 
 #' bm <-getMask( antsImageRead( getANTsRData("ch2") ) )
 #' brain <-renderSurfaceFunction( surfimg =list( bm ) , alphasurf=0.1, smoothsval = 1.5 )
 #' fullView(tempfile( fileext='.png'))
 #'
 #' @export fullView
+fullView <- function(SurfaceImage, FunctionalImage, views, slice, axis, statdir){
+  DIM <- dim(img)
+  nviews <- length(views)
+  if (!missing(slice)) {
+    if (axis == 1) {
+      left_sagittal <- img[1:slice, 1:DIM[2], 1:DIM[3]]
+      left_sagittal[slice[1],,] <- 0
+      right_sagittal <- img[slice:DIM[1], 1:DIM[2], 1:DIM[3]]
+      right_sagittal[,slice,] <- 0
+    } else if (axis == 2) {
+      superior_axial <- img[1:DIM[1], 1:DIM[2], 1:slice]
+      superior_axial[,, slice] <- 0
+      inferior_axial <- img[1:DIM[1], 1:DIM[2], slice:DIM[3]]
+      inferior_axial[,, slice] <- 0
+    } else if (axis == 3) {
+      posterior_coronal <- img[1:DIM[1], 1:slice, 1:DIM[3]]
+      posterior_coronal[, slice,] <- 0
+      anterior_coronal <- img[1:DIM[1], slice:DIM, 1:DIM[3]]
+      anterior_coronal[, slice,] <- 0
+    }
+  }
+  
 
-fullView <- function(statdir){
   
   # create indicies of rotation--------------------------
   x90p <- rotationMatrix(pi/2, 1, 0, 0)
@@ -24,93 +47,104 @@ fullView <- function(statdir){
   z180n <- rotationMatrix(-pi, 0, 0, 1)
   
   # extract each viewpoint
-  rgl::par3d(userMatrix = x90n) # anterior view
-  rgl::rgl.snapshot(paste(statdir, "anterior.png", sep = ""), fmt = "png",
-                    top = TRUE)
-  rgl::par3d(userMatrix = x90n %*% z90n) # left view
-  rgl::rgl.snapshot(paste(statdir, "left.png", sep = ""), fmt = "png",
-                    top = TRUE)
-  rgl::par3d(userMatrix = x90n %*% z90p) # right view
-  rgl::rgl.snapshot(paste(statdir, "right.png", sep = ""), fmt = "png",
-                    top = TRUE)
-  rgl::par3d(userMatrix = x180p) # inferior view
-  rgl::rgl.snapshot(paste(statdir, "inferior.png", sep = ""), fmt = "png",
-                    top = TRUE)
-  rgl::par3d(userMatrix = x90p %*% y180p) # posterior view
-  rgl::rgl.snapshot(paste(statdir, "posterior.png", sep = ""), fmt = "png",
-                    top = TRUE)
-  rgl::par3d(userMatrix = z180p) # superior view
-  rgl::rgl.snapshot(paste(statdir, "superior.png", sep = ""), fmt = "png",
-                    top = TRUE)
-  
+  if (any(views == "right_sagittal")) {
+    rgl::par3d(userMatrix = x90n %*% z90n) # left view
+    rgl::rgl.snapshot(paste(statdir, "left.png", sep = ""), fmt = "png", top = TRUE)
+  }
+  if (any(views == "left_sagittal")) {
+    rgl::par3d(userMatrix = x90n %*% z90p) # right view
+    rgl::rgl.snapshot(paste(statdir, "right.png", sep = ""), fmt = "png", top = TRUE)
+  }
+  if (any(views == "anterior")) {
+    rgl::par3d(userMatrix = x90n) # anterior view
+    rgl::rgl.snapshot(paste(statdir, "anterior.png", sep = ""), fmt = "png", top = TRUE)
+    aa <- png::readPNG(paste(statdir, "anterior.png", sep = ""))
+    png(paste(statdir, "anterior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
+    grid::grid.raster(aa)
+    grid::grid.text("Anterior", .5, .95)
+    grid::grid.text("L", .9, .5) 
+    grid::grid.text("R", .1, .5)
+    grid::grid.text("S", .5, .9)
+    grid::grid.text("I", .5, .1)
+    dev.off()
+  }
+  if (any(views == "lef")) {
+    rgl::par3d(userMatrix = x90n %*% z90n) # left view
+    rgl::rgl.snapshot(paste(statdir, "left.png", sep = ""), fmt = "png", top = TRUE)
+    bb <- png::readPNG(paste(statdir, "left.png", sep = ""))
+    png(paste(statdir, "left.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
+    grid::grid.raster(bb)
+    grid::grid.text("Left", .5, .95)
+    grid::grid.text("A", .9, .5) 
+    grid::grid.text("P", .1, .5)
+    grid::grid.text("S", .5, .9)
+    grid::grid.text("I", .5, .1)
+    dev.off()
+  }
+  if (any(views == "right")) {
+    rgl::par3d(userMatrix = x90n %*% z90p) # right view
+    rgl::rgl.snapshot(paste(statdir, "right.png", sep = ""), fmt = "png", top = TRUE)
+    cc <- png::readPNG(paste(statdir, "right.png", sep = ""))
+    png(paste(statdir, "right.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
+    grid::grid.raster(cc)
+    grid::grid.text("Right", .5, .95)
+    grid::grid.text("P", .9, .5)
+    grid::grid.text("A", .1, .5)
+    grid::grid.text("S", .5, .9)
+    grid::grid.text("I", .5, .1)
+    dev.off()
+  }
+  if (any(views == "inferior")) {
+    rgl::par3d(userMatrix = x180p) # inferior view
+    rgl::rgl.snapshot(paste(statdir, "inferior.png", sep = ""), fmt = "png", top = TRUE)
+    dd <- png::readPNG(paste(statdir, "inferior.png", sep = ""))
+    png(paste(statdir, "inferior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
+    grid::grid.raster(dd)
+    grid::grid.text("Inferior", .5, .95)
+    grid::grid.text("L", .9, .5) 
+    grid::grid.text("R", .1, .5)
+    grid::grid.text("A", .5, .9)
+    grid::grid.text("P", .5, .1)
+    dev.off()
+  }
+  if (any(views == "posterior")) {
+    rgl::par3d(userMatrix = x90p %*% y180p) # posterior view
+    rgl::rgl.snapshot(paste(statdir, "posterior.png", sep = ""), fmt = "png", top = TRUE)
+    ee <- png::readPNG(paste(statdir, "posterior.png", sep = ""))
+    png(paste(statdir, "posterior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
+    grid::grid.raster(ee)
+    grid::grid.text("Posterior", .5, .95)
+    grid::grid.text("R", .9, .5) 
+    grid::grid.text("L", .1, .5)
+    grid::grid.text("S", .5, .9)
+    grid::grid.text("I", .5, .1)
+    dev.off()
+  }
+  if (any(views == "superior")) {
+    rgl::par3d(userMatrix = z180p) # superior view
+    rgl::rgl.snapshot(paste(statdir, "superior.png", sep = ""), fmt = "png", top = TRUE)
+    ff <- png::readPNG(paste(statdir, "superior.png", sep = ""))
+    png(paste(statdir, "superior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
+    grid::grid.raster(bb)
+    grid::grid.text("Superior", .5, .95)
+    grid::grid.text("R", .9, .5) 
+    grid::grid.text("L", .1, .5)
+    grid::grid.text("A", .5, .9)
+    grid::grid.text("P", .5, .1)
+    dev.off()
+  }
   # Create anterior view image
-  aa <- png::readPNG(paste(statdir, "anterior.png", sep = ""))
   
   #lim$usr[1] = left x
   #lim$usr[2] = y bottom
   #lim$usr[3] = right x
   #lim$usr[4] = y top
   
-  png(paste(statdir, "anterior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
-  grid::grid.raster(aa)
-  grid::grid.text("Anterior", .5, .95)
-  grid::grid.text("L", .9, .5) 
-  grid::grid.text("R", .1, .5)
-  grid::grid.text("S", .5, .9)
-  grid::grid.text("I", .5, .1)
-  dev.off()
-  
-  bb <- png::readPNG(paste(statdir, "left.png", sep = ""))
-  png(paste(statdir, "left.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
-  grid::grid.raster(bb)
-  grid::grid.text("Left", .5, .95)
-  grid::grid.text("A", .9, .5) 
-  grid::grid.text("P", .1, .5)
-  grid::grid.text("S", .5, .9)
-  grid::grid.text("I", .5, .1)
-  dev.off()
-  
-  cc <- png::readPNG(paste(statdir, "right.png", sep = ""))
-  png(paste(statdir, "right.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
-  grid::grid.raster(cc)
-  grid::grid.text("Right", .5, .95)
-  grid::grid.text("P", .9, .5)
-  grid::grid.text("A", .1, .5)
-  grid::grid.text("S", .5, .9)
-  grid::grid.text("I", .5, .1)
-  dev.off()
-  
-  dd <- png::readPNG(paste(statdir, "inferior.png", sep = ""))
-  png(paste(statdir, "inferior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
-  grid::grid.raster(dd)
-  grid::grid.text("Inferior", .5, .95)
-  grid::grid.text("L", .9, .5) 
-  grid::grid.text("R", .1, .5)
-  grid::grid.text("A", .5, .9)
-  grid::grid.text("P", .5, .1)
-  dev.off()
-  
-  ee <- png::readPNG(paste(statdir, "posterior.png", sep = ""))
-  png(paste(statdir, "posterior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
-  grid::grid.raster(ee)
-  grid::grid.text("Posterior", .5, .95)
-  grid::grid.text("R", .9, .5) 
-  grid::grid.text("L", .1, .5)
-  grid::grid.text("S", .5, .9)
-  grid::grid.text("I", .5, .1)
-  dev.off()
-  
-  ff <- png::readPNG(paste(statdir, "superior.png", sep = ""))
-  png(paste(statdir, "superior.png", sep = ""), width = dim(aa)[2], height = dim(aa)[1])
-  grid::grid.raster(bb)
-  grid::grid.text("Superior", .5, .95)
-  grid::grid.text("R", .9, .5) 
-  grid::grid.text("L", .1, .5)
-  grid::grid.text("A", .5, .9)
-  grid::grid.text("P", .5, .1)
-  dev.off()
-  
-  aa <- png::readPNG(paste(statdir, "anterior.png", sep = ""))
+  for (i in 1:nviews) {
+    view_name <- paste(statdir, views[i], ".png", sep = "")
+    mypic <- png::readPNG(view_name)
+    if (i ==2 ) 
+    
   bb <- png::readPNG(paste(statdir, "left.png", sep = ""))
   cc <- png::readPNG(paste(statdir, "right.png", sep = ""))
   dd <- png::readPNG(paste(statdir, "inferior.png", sep = ""))
