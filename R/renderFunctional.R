ec95b208c373a29a20746af410d7ba24fededf2f
Tokazama <zchristensen7@gmail.com>
2016-03-25 14:41:23 -0600

    Update renderFunctional.R

--- R/renderFunctional.R
+++ R/renderFunctional.R
@@ -26,18 +26,26 @@
 #'
 #'
 #' @export renderFunctional
-renderFunctional <- function(funcimg, surfval = .5, colorGradient = "rainbow", max, min = 0, upper = 1, lower = 1, alpha = 1, smoothval = 0, material = "metal", depth = .6, verbose = TRUE) {
-  img <- antsImageClone(funcimg)
+renderFunctional <- function(funcimg, colorGradient = "rainbow", max, min = 1, upper = 1, lower = 1, alpha = 1, smoothval, material = "metal", depth = .6, verbose = TRUE) {
+  if (missing(smoothval))
+    img <- antsImageClone(funcimg)
+  else
+    img <- smoothImage(funcimg, smoothval)
+
   # Perona malik edge preserving smoothing
   # iMath(img, "PeronaMalike", iterations (ex. 10), conductance (ex. .5) 
-  
-  img <- smoothImage(funcimg, smoothval)
-  mask <- antsImageClone(img)
-  mask[mask != 0] <- 1
-  mask <- iMath(mask, "FillHoles")
-  emask <- iMath(mask, "ME", 1)
-  surf <- as.array(mask - emask)
-  xyz <- which(surf != 0, arr.ind = TRUE)
+  origin <- antsGetOrigin(img)
+  # DIM <- dim(img)
+  # 
+  # as.antsImage(img[1:origin[1], 1:DIM[2], 1:DIM[3]])
+  # coronal_post <- img[1:DIM[1], 1:origin[2], 1:DIM[3]]
+  # coronal_post <- img[1:DIM[1], origin[2]:DIM[2], 1:DIM[3]]
+  # 
+  # left_sagittal <- img[1:origin[1], 1:DIM[2], 1:DIM[3]]
+  # right_sagittal <- img[origin[1]:DIM[1], 1:DIM[2], 1:DIM[3]]
+  # 
+  # superior_axial
+  # inferior_axial
   func <- as.array(img)
   unique_vox <- length(unique(func))
   if (missing(max)) {
@@ -47,8 +55,7 @@ renderFunctional <- function(funcimg, surfval = .5, colorGradient = "rainbow", m
       max <- unique_vox
   }
   func <- round((func - min(func)) / max(func - min(func)) * (max - min) + min)
-  vox_val <- as.numeric(func, sur > 0)
-  
+  vox_vec <- as.numeric(func, func > 0)
   
   # create color lookup table--------------------------------------------------
   if (verbose)
@@ -73,64 +80,42 @@ renderFunctional <- function(funcimg, surfval = .5, colorGradient = "rainbow", m
     mycolors <- grey.colors(max, alpha = 0)
   else 
     mycolors <- colorGradient
-  
   # white out upper or lower percentage of image
   if (lower < 1)
     mycolors[1:floor(lower * max)] <- "white"
   if (upper < 1)
     mycolors[ceiling(upper * max):max] <- "white"
+  cols <- mycolors[vox_vec]
   
   # render surface-------------------------------------------------------------
   if (verbose)
     cat("Computing surface \n")
-  brain <- misc3d::contour3d(mask, level = surfval, alpha = alpha, draw = FALSE, smooth = 1, material = material, depth = depth)
-
-  if (length(colorGradient > 1)) {
-    n <- length(brain$v1[, 1])
-    vox_vec <- rep(0, n)
-    findColor <- function(x) {
-      surf_img[floor(x[i, 1]), floor(x[i, 2]), floor(x[i, 2])]
+  if (verbose)
+        progress <- txtProgressBar(min = 0, max = max, style = 3)
+  level_seq <- c(min:max)
+  for (i in 1:max) {
+    eps <- .Machine$double.eps
+    tmp <- array(FALSE, dim = (dim(img)))
+    mlev <- level_seq
+    if (i == max)
+      nlev <- max + eps
+    else
+      nlev <- level_seq[i + 1]
+    tmp[ func >= mlev & func < nlev ] <- 1
+    if (sum(tmp != 0, na.rm=TRUE) == 0) {
+      warning("No contour to make")
+      next
+    } else {
+    brain <- contour3d(tmp, level = 0, alpha = alpha[i], color = cols[i], draw = FALSE)
+    brain$v1 <- antsTransformIndexToPhysicalPoint(img, brain$v1)
+    brain$v2 <- antsTransformIndexToPhysicalPoint(img, brain$v2)
+    brain$v3 <- antsTransformIndexToPhysicalPoint(img, brain$v3)
     }
-    brain$color <- apply(brain$v1, 1, findColor)
-    brain$color2 <- apply(brain$v2, 1, findColor)
-    brain$color.mesh <- apply(brain$v3, 1, findColor)
-  } else {
-    brain$color <- mycolors
-    # surf_img$color2 <- mycolors[myv2]
-    # surf_img$col.mesh <- mycolors[myv3]
+    if (verbose)
+      setTxtProgressBar(progress, i)
   }
-    
-  brain$v1 <- antsTransformIndexToPhysicalPoint(img, brain$v1)
-  brain$v2 <- antsTransformIndexToPhysicalPoint(img, brain$v2)
-  brain$v3 <- antsTransformIndexToPhysicalPoint(img, brain$v3)
-  
-  .check3d()
-  misc3d::drawScene.rgl(brain)
-  brain
+  if (verbose)
+      close(progress)
+  scene <- c(scene, list(brain))
+  scene
 }
-
-origin <- antsGetOrigin(img)
-DIM <- dim(img)
-
-as.antsImage(img[1:origin[1], 1:DIM[2], 1:DIM[3]])
-coronal_post <- img[1:DIM[1], 1:origin[2], 1:DIM[3]]
-coronal_post <- img[1:DIM[1], origin[2]:DIM[2], 1:DIM[3]]
-
-left_sagittal <- img[1:origin[1], 1:DIM[2], 1:DIM[3]]
-right_sagittal <- img[origin[1]:DIM[1], 1:DIM[2], 1:DIM[3]]
-
-superior_axial 
-inferior_axial
-
-
-
-
-
-img <- smoothImage(funcimg, smoothval)
-mask <- antsImageClone(img)
-mask[mask != 0] <- 1
-mask <- iMath(mask, "FillHoles")
-emask <- iMath(mask, "ME", 1)
-surf <- as.array(mask - emask)
-xyz <- which(surf != 0, arr.ind = TRUE)
-spheres3d(xyz[, 1], xyz[, 2], xyz[, 3])
