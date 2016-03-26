46c23c4ca03fd8886e066516c24d7cbfda044a09
Tokazama <zchristensen7@gmail.com>
2016-03-26 08:24:48 -0600

    Update renderFunctional.R

--- R/renderFunctional.R
+++ R/renderFunctional.R
@@ -12,7 +12,6 @@
 #' \item{colorRamp: } {custom colorRamp function}
 #' }
 #' @param max integer. upper end of image scale
-#' @param min integer. lower end of image scale
 #' @param upper fraction of upper values to use
 #' @param lower fraction of lower values to use
 #' @param alpha opacity
@@ -24,11 +23,18 @@
 #'
 #'
 #' @export renderFunctional
-renderFunctional <- function(ilist, color, max, min = 1, upper = 1, lower = 1, alpha = 1, slice = NULL, axis, smoothval, material = "metal") {
-  DIM <- dim(surfimg)
+renderFunctional <- function(ilist, color, max, upper = 1, lower = 1, alpha = 1, slice = NULL, axis, smoothval, material = "metal") {
+  DIM <- dim(ilist[[i]])
   nimg <- length(ilist)
+  if (missing(max)) {
+    max <- rep(0, nimg)
+  } else if (length(max) != nimg) {
+    cat("Length of max doesn't equal number of images. Resetting max.")
+    max <- rep(0, nimg)
+  }
   part1 <- list()
   part2 <- list()
+  eps <- .Machine$double.eps
   for (i in 1:nimg) {
     if (is.null(slice)) {
       if (missing(smoothval))
@@ -58,19 +64,19 @@ renderFunctional <- function(ilist, color, max, min = 1, upper = 1, lower = 1, a
         part2[[i]][, slice,] <- 0
       }
     }
-    # Perona malik edge preserving smoothing
-    # iMath(img, "PeronaMalike", iterations (ex. 10), conductance (ex. .5) 
-    for (ifunc in 1:ilist) {
-    unique_vox <- length(unique(func))
-    if (missing(max)) {
-      if (unique_vox > 2^8)
-        max <- 2^8
+  }
+  # Perona malik edge preserving smoothing
+  # iMath(img, "PeronaMalike", iterations (ex. 10), conductance (ex. .5) 
+  for (ifunc in 1:ilist) {
+    unique_vox <- length(unique(ilist[[ifunc]]))
+    if (missing(max[[ifunc]])) {
+      if (unique_vox > 2^7)
+        max[[ifunc]] <- 2^7
       else
-        max <- unique_vox
+        ax[[ifunc]] <- unique_vox
     }
-    func <- round((func - min(func)) / max(func - min(func)) * (max - min) + min)
-    vox_vec <- as.numeric(func, func > 0)
-    
+    func <- round((ilist[[ifunc]] - min(ilist[[ifunc]])) / max(ilist[[ifunc]] - min(ilist[[ifunc]])) * (max[[ifunc]] - 1) + 1)
+
     # create color lookup table--------------------------------------------------
     if (verbose)
       cat("Creating color palette. \n")
@@ -78,59 +84,49 @@ renderFunctional <- function(ilist, color, max, min = 1, upper = 1, lower = 1, a
     brain.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
                                        "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"),
                                      interpolate = c("spline"), space = "Lab")
-    if (colorGradient[1] == "terrain") 
-      mycolors <- terrain.colors(max, alpha = 0)
-    else if (colorGradient[1]  == "heat") 
-      mycolors <- heat.colors(max, alpha = 0)
-    else if (colorGradient[1]  == "topo") 
-      mycolors <- topo.colors(max, alpha = 0)
-    else if (colorGradient[1]  == "cm") 
-      mycolors <- cm.colors(max, alpha = 0)
-    else if (colorGradient[1] == "brain") 
-      mycolors <- brain.colors(max, alpha = 0)
-    else if (colorGradient[1] == "rainbow") 
-      mycolors <- rainbow(max, alpha = 0)
-    else if (colorGradient[1] == "gs") 
-      mycolors <- grey.colors(max, alpha = 0)
-    else 
+    if (colorGradient[1] == "terrain")
+      mycolors <- terrain.colors(max[[ifunc]], alpha = 0)
+    else if (colorGradient[1]  == "heat")
+      mycolors <- heat.colors(max[[ifunc]], alpha = 0)
+    else if (colorGradient[1]  == "topo")
+      mycolors <- topo.colors(max[[ifunc]], alpha = 0)
+    else if (colorGradient[1]  == "cm")
+      mycolors <- cm.colors(max[[ifunc]], alpha = 0)
+    else if (colorGradient[1] == "brain")
+      mycolors <- brain.colors(max[[ifunc]], alpha = 0)
+    else if (colorGradient[1] == "rainbow")
+      mycolors <- rainbow(max[[ifunc]], alpha = 0)
+    else if (colorGradient[1] == "gs")
+      mycolors <- grey.colors(max[[ifunc]], alpha = 0)
+    else
       mycolors <- colorGradient
     # white out upper or lower percentage of image
     if (lower < 1)
-      mycolors[1:floor(lower * max)] <- "white"
+      mycolors[1:floor(lower * max[[ifunc]])] <- "white"
     if (upper < 1)
-      mycolors[ceiling(upper * max):max] <- "white"
-    cols <- mycolors[vox_vec]
-    
+      mycolors[ceiling(upper * max[[ifunc]]):max[[ifunc]]] <- "white"
+
     # render surface-------------------------------------------------------------
     if (verbose)
       cat("Computing surface \n")
     if (verbose)
-          progress <- txtProgressBar(min = 0, max = max, style = 3)
-    level_seq <- c(min:max)
-    for (i in 1:max) {
-      eps <- .Machine$double.eps
-      tmp <- array(FALSE, dim = (dim(img)))
-      mlev <- level_seq
-      if (i == max)
-        nlev <- max + eps
-      else
-        nlev <- level_seq[i + 1]
-      tmp[ func >= mlev & func < nlev ] <- 1
-      if (sum(tmp != 0, na.rm=TRUE) == 0) {
-        warning("No contour to make")
-        next
-      } else {
-      brain <- contour3d(tmp, level = 0, alpha = alpha[i], color = cols[i], draw = FALSE)
-      brain$v1 <- antsTransformIndexToPhysicalPoint(img, brain$v1)
-      brain$v2 <- antsTransformIndexToPhysicalPoint(img, brain$v2)
-      brain$v3 <- antsTransformIndexToPhysicalPoint(img, brain$v3)
-      }
-      scene <- c(scene, list(brain))
+      progress <- txtProgressBar(min = 0, max = max[[ifunc]], style = 3)
+    level_seq <- c(1:max[[ifunc]])
+      for (i in 1:max[[ifunc]]) {
+        if (any(ilist[[ifunc]] == level_seq[i])) {
+          tmp <- array(0, dim = DIM)
+          tmp[ ilist[[ifunc]] == level_seq[[i]] ] <- 1
+          brain <- contour3d(tmp, level = 0, alpha = alpha[i], color = mycolors[i], draw = FALSE)
+          brain$v1 <- antsTransformIndexToPhysicalPoint(img, brain$v1)
+          brain$v2 <- antsTransformIndexToPhysicalPoint(img, brain$v2)
+          brain$v3 <- antsTransformIndexToPhysicalPoint(img, brain$v3)
+          scene <- c(scene, list(brain))
+        }
       if (verbose)
         setTxtProgressBar(progress, i)
+      }
     }
     if (verbose)
-        close(progress)
+      close(progress)
+    scene
   }
-  scene
-}
