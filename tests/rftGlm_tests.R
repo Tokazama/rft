e22bd7ccdd7721408615b5c967a41fa3a94551d0
Tokazama <zchristensen7@gmail.com>
2016-06-08 19:59:58 -0700

    Create rftGlm_tests.R

---
+++ tests/rftGlm_tests.R
@@ -0,0 +1,29 @@
+# read in data
+filepath <- '/Volumes/SANDISK/datasets/ptbp/rft/warp/'
+ifiles <- file.path(filepath, list.files(filepath)[1:33])
+mask <- getMask(antsImageRead(ifiles[1]))
+imat <- imagesToMatrix(ifiles, mask)
+ptbp <- read.csv('/Volumes/SANDISK/datasets/ptbp/data/ptbp_summary_demographics.csv')
+X <- model.matrix(~ptbp$AgeAtScan)
+
+# rftModel
+fm1 <- rftModel(imat, X, mask, contrastMatrix, diag(n), findVar = TRUE, findResels = FALSE, "rftGlm")
+
+
+# update
+
+# residuals
+
+# rftREML
+
+# rftGlm
+
+# summary
+
+# anova
+
+# predict
+
+# predict.summary
+
+# predict.anova
