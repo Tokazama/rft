9cd8e899e629972efcd440d71a20660710977083
Tokazama <zchristensen7@gmail.com>
2016-06-09 11:16:24 -0700

    Update rftGlm_tests.R

--- tests/rftGlm_tests.R
+++ tests/rftGlm_tests.R
@@ -5,6 +5,7 @@ mask <- getMask(antsImageRead(ifiles[1]))
 imat <- imagesToMatrix(ifiles, mask)
 ptbp <- read.csv('/Volumes/SANDISK/datasets/ptbp/data/ptbp_summary_demographics.csv')
 X <- model.matrix(~ptbp$AgeAtScan)
+conmat <- matrix(c(0, 1, 0, -1), 2, 2, byrow = TRUE)
 
 # rftModel
 fm1 <- rftModel(imat, X, mask, contrastMatrix, diag(n), findVar = TRUE, findResels = FALSE, "rftGlm")
