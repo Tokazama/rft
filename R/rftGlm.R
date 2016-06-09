bec1d803c11d375640d437d288dbcf87d0d919cb
Tokazama <zchristensen7@gmail.com>
2016-06-08 19:40:41 -0700

    print.rftModel and rftModel function incorporated

--- R/rftGlm.R
+++ R/rftGlm.R
@@ -1,77 +1,115 @@
-#' ICSNP has hotelling's T-test
+#' Fit images to a generalized linear model using random field theory
+#' 
+#' @param images either an image matrix or image list
+#' @param RH the right hand side of a formula (i.e. " ~ pred1 + pred2)
+#' @param intercept should the design matrix include an intercept. logical (default = FALSE)
+#' @param mask binary mask of class antsImage
+#' @param data an optional data frame, list or environment (or object coercible by as.data.frame to a data frame) containing the variables in the model. If not found in data, the variables are taken from environment(formula), typically the environment from which rftGlm is called.
+#' @param contrastMatrix a matrix in which each row represents a unique contrast and each column the weights for each predictor
+#' @param fieldType the type of contrast to compute corresponding to each row of contrastMatrix ("F" for F-statistic & "T" for t-statistic)
+#' @param statdir optional directory to save results
+#' @param sample the amount of residual images used to estimate smoothness parameters (default uses all residuals)
+#' @param verbose
+#' @param smooth_sigma
+#' @param filterfun filter/whiten function used to filter response and predictors prior to fitting (i.e. filterfun = whiten; default filterfun = NULL)
+#' @param weights vector of weights corresponding to each row of the design matrix and response matrix
+#' @param V optional non-sphericity matrix (if not specified this is estimated)
+#' @param control a list of parameters for controlling the fitting process. (see rftControl)
+#' 
+#' @return
+#' \item{call} {the matched call}
+#' \item{dof} {degrees of freedom}
+#' \item{weights} {weight matrix where the diagonal corresponds to each rows weight}
+#' \item{coefficients} {model coefficients}
+#' \item{rss} {residual sum of squares}
+#' \item{residuals} {a matrix of residuals for each image or the directory where they were saved (if statdir was specified)}
+#' \item{parameters}
+#' \itemize{
+#'   \item{S} {array of non-sphericity components}
+#'   \item{V} {non-sphericity matrix}
+#'   \item{h} {hyperparameters}
+#'   \item{Cy} {covariance matrix of images}
+#'   \item{R} {residual forming projection matrix}
+#'   }
+#' \item{volumes}
+#' \itemize{
+#'   \item{mask}
+#'   \item{rpvImage}
+#'   \item{fwhm}
+#'   \item{resels}
+#'   \item{voxels}
+#'   \item{nimg}
+#'   \item{npred}
+#'   }
+#' \item{contrasts}
+#' \itemize{
+#'   \item{contrastMatrix}
+#'   \item{fieldType}
+#' }
+#' \item{statdir}
+#' @details
+#' 
+#' 
+#' Internal functions:
+#' glFormula      rftModel
+#' mkGlmerDevFun  rftREML
+#' optimizeGlmer  rftREML
+#' updateGlmer    update.rftModel
+#' mkMerMod       rftModel
+#' 
+#' @author Zachary P. Christensen
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
 #' @export rftGlm
-
-rftGlm <- function(formula, mask, data, contrast, conType, statdir = NULL, sample = NULL, verbose = TRUE,...) {
-  if (missing(data))
-    data <- environment(formula)
-  cl <- match.call()
-  X <- model.matrix(formula, data = data)
-  y <- model.response(model.frame(formula, data = data))
-
-  v <- ncol(y)
-  n <- nrow(y)
-  p <- ncol(X)
-  edf <- p - 1
-  rdf <- n - p
+rftGlm <- function(images, RH, intercept = FALSE, mask, data = NULL, contrastMatrix = NULL, statdir = NULL, sample = NULL, verbose = TRUE) {
   
-  b   <- matrix(0, nrow = p, ncol = v) # coefficients
-  rownames(b) <- colnames(X)
-  rss <- matrix(0, nrow = 1, ncol = v) # residual sum of squares
-  r   <- matrix(0, nrow = n, ncol = v) # residuals
+  if (class(images) == "matrix") {
+    n <- dim(images)[[1]]
+    v <- dim(images)[[2]]
+  } else {
+    n <- length(images)
+    v <- sum(mask)
+  }
   
-  # fit each voxel-------------------------------------------------------------
-  if (verbose) {
-    cat("Fitting model. \n")
-    progress <- txtProgressBar(min = 0, max = v, style = 3)
+  
+  # estimate parameters--------------------------------------------------------
+  if (is.null(data))
+    data <- environment(RH)
+  X <- model.matrix(update(RH, ~ . - 1), data = data)
+  
+  # fit model------------------------------------------------------------------
+  if (missing(weights)) {
+    mymod <- rftModel(y, X, newmask, contrastMatrix, diag(n), findVar = TRUE, findResels = FALSE, "rftGlm")
+    V <- rftREML(mymod$Cy, X, mymod$V)$V
+    iV <- sqrt(MASS::ginv(V))
+    weights <- iV * (abs(iV) > 1e-6)
   }
-  for (i in 1:v) {
-    fit <- glm.fit(X, y[, i], ...)
-    b[, i]   <- fit$coefficients
-    rss[, i] <- sum(fit$residuals^2)
-    r[, i]   <- fit$residuals / sqrt(rss[, i] / rdf) # scale residuals with mean residual sum of squares
-    if (verbose)
-      setTxtProgressBar(progress, i)
-  }
-  if (verbose)
-    close(progress)
+  mymod <- update(mymod, weights = weights, findResels = TRUE, findVar = FALSE)
+  mymod$call <- call
+  mymod
+}
 
-  # estimate fwhm/resels-------------------------------------------------------
-  if (!is.null(statdir)) {
-    rl <- paste(statdir, "residual_", 1:n, ".nii.gz", sep = "")
-    if (verbose)
-      cat("Writing residual images to director. \n")
-    for (i in 1:n) {
-      antsImageWrite(makeImage(mask, r[i, ]), rl[i])
-    }
-    r <- rl
-  }
-  if (verbose)
-    cat("Estimating FWHM. \n")
-  smooth <- estSmooth(r, mask, rdf, scaleResid = FALSE, sample = sample, verbose = verbose)
-  if (verbose)
-    cat("Estimating resels. \n")
-  v2r <- resels(mask, smooth$fwhm)
+#' @export print.rftModel
+print.rftModel <- function(x) {
+  cat("Call: \n")
+  cat(x$call, "\n\n")
   
-  if (missing(contrast))
-    contrast <- NULL
-  if (missing(conType))
-    conType <- NULL
-  # create rft object-----------------------------------------------------
-  z <- structure(list(call = cl,
-                      coefficients = b,
-                      residuals = r,
-                      rss = rss,
-                      RPVImage = smooth$RPVImage,
-                      fwhm = smooth$fwhm,
-                      resels = v2r,
-                      mask = antsImageClone(mask),
-                      df = c(edf, rdf),
-                      dm = X,
-                      contrast = contrast,
-                      contrast.type = conType,
-                      voxels = v,
-                      statdir = statdir,
-                      method = "rftGlm"),
-                 class = "rft")
-  z
+  cat("Coefficients: \n")
+  ncoef <- nrow(x$beta)
+  for (i in 1:ncoef) {
+    cat("  ", colnames(x$coefficients)[i], "\n")
+  }
+  cat("\n")
+  
+  cat("Degrees of freedom = ", x$dof, "\n")
+  cat("Voxels = ", x$dims$vn, "\n")
+  cat("FWHM = ", x$dims$fwhm, "\n")
+  cat("Resels = ", x$dims$resels, "\n\n")
+  
+  cat("Directory: ", x$statdir, "\n")
+  
 }
