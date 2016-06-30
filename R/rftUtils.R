d8349d9f0e27cb0f5a2296f095a5eee65c7d49a4
Tokazama <zchristensen7@gmail.com>
2016-06-30 15:30:44 -0600

    Update rftUtils.R

--- R/rftUtils.R
+++ R/rftUtils.R
@@ -1,5 +1,227 @@
-
 ## from spm_sp
+## to do:
+## isspc
+## issetspc
+## size = just gives the dimensions of x$X
+isinsp = function(x, c, tol) {
+  'is in space or is in dual space'
+  if (missing(tol))
+    tol <- x$tol
+  if (nrow(x$X) != nrow(c)) {
+    warning('Vector dim don''t match col dim : not in space')
+    return(0)
+  } else {
+    out <- all(abs(util_op(x) %*% c - c) <= tol)
+    return(colSums(out) > 0)
+  }
+}
+
+isinspp = function(x, c, tol) {
+  'is in space or is in dual space'
+  if (missing(tol))
+    tol <- x$tol
+  if (ncol(x$X) != nrow(c)) {
+    warning('Vector dim don''t match row dim : not in space')
+    return(0)
+  } else {
+    out <- all(abs(util_opp(x) %*% c - c) <= tol)
+    return(colSums(out) > 0)
+  }
+}
+
+eachinsp = function(x, c, tol) {
+  'each column of c in space or in dual space'
+  if (missing(tol))
+    tol <- x$tol
+  if (nrow(x$X) != nrow(c)) {
+    warning('Vector dim don''t match col. dim : not in space')
+    return(0)
+  } else
+    return(all(abs(util_op(x) %*% c - c) <= tol))
+}
+
+eachinspp = function(x, c, tol) {
+  'each column of c in space or in dual space'
+  if (missing(tol))
+    tol <- x$tol
+  if (ncol(x$X) != nrow(c)) {
+    warning('Vector dim don''t match row. dim : not in space')
+    return(0)
+  } else
+    return(all(abs(util_opp(x) %*% c - c) <= tol))
+}
+
+util_equal = function(x, X2) {
+  'test wether two spaces are the same'
+  x2 <- util_set(X2)
+  maxtol <- max(x$tol, x2$tol)
+  return(all(util_isinsp, x, X2, maxtol) && all(util_isinsp(x2, x$X, maxtol))
+}
+
+check = function(x, y, tol) {
+  'set y and tol according to arguments'
+  if (missing(tol))
+    tol <- x$tol
+  if (missing(y))
+    return(util_tol(x, tol))
+  else
+    return(util_tol(y, tol))
+}
+
+util_nop = function(x, y, check) {
+  'project into null space'
+  n <- util_n(x)
+  if (missing(y))
+    n <- tcrossprod(n)
+  else
+    n <- n %*% crossprod(n, y)
+  if (check)
+    return(util_tol(n, x$tol))
+  else
+    return(n)
+}
+
+util_res = function(x, y, check) {
+  if (missing(y))
+    out <- util_r(x)
+  else
+    out <- util_rY(x, y)
+  if (check)
+    return(util_tol(out, x$tol))
+  else
+    return(out)
+}
+
+util_carrotp <- function(x, n, y, check) {
+  if (missing(n))
+    n <- 1
+  if (missing(y))
+    n <- util_jbp(x, n)
+  else
+    n <- util_jbp(x, n) %*% y
+  if (check)
+    return(util_tol(n, x$tol))
+  else
+    return(n)
+}
+
+util_carrot <- function(x, n, y, check) {
+  if (missing(n))
+    n <- 1
+  if (missing(y))
+    out <- util_jb(x, n)
+  else
+    out <- util_jb(x, n, y)
+  if (check)
+    return(util_tol(out, x$tol))
+  else
+    return(out)
+}
+
+util_np <- function(x) {
+  'null space of x transposed'
+  util_n(util_transp(x))j
+}
+
+util_opColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_op(x), x$tol)
+  else
+    util_tol(util_op(x) %*% y, x$tol)
+}
+
+util_oppColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_opp(x), x$tol)
+  else
+    util_tol(util_opp(x) %*% y, x$tol)
+}
+
+util_xDash <- function(x, y) {
+  if (missing(y))
+    util_pinv(x)
+  else
+    util_pinv(x) %*% y
+}
+
+util_xDashColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_pinv(x), util_t(x))
+  else
+    util_tol(util_pinv(x) %*% y, util_t(x))
+}
+
+util_xpDash <- function(x, y) {
+  if (missing(y))
+    util_pinvxp(x)
+  else
+    util_pinvxp(x) %*% y
+}
+
+util_xpDashColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_pinvxp(x), util_t(x))
+  else
+    util_tol(util_pinvxp(x) %*% y, util_t(x))
+}
+
+util_cukxpDash <- function(x, y) {
+  if (missing(y))
+    util_cukpinvxp(x)
+  else
+    util_cukpinvxp(x) %*% y
+}
+
+util_cukxpDashColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_cukpinvxp(x), x$tol)
+  else
+    util_tol(util_cukpinvxp(x) %*% y, x$tol)
+}
+
+util_cukxColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_cukx(x), x$tol)
+  else
+    util_tol(util_cukx(x, y), x$tol)
+}
+
+util_ox <- function(x) {
+  if (util_rk(x) > 0)
+    util_uk(x)
+  else
+    matrix(0, util_s1(x), 1)
+}
+
+util_oxp <- function(x) {
+  if (util_rk(x) > )
+    util_vk(x)
+  else
+    matrix(0, util_s2(x), 1)
+}
+
+util_xi <- function(x, i) {
+  x$X[, i]
+}
+
+util_xpi <- function(x, i) {
+  t(x$X[i, ])
+}
+
+util_cx2cu <- function(x, y) {
+  if (missing(y))
+    util_cxtwdcu(x)
+  else
+    util_cxtwdcu(x) %*% y
+}
+
+util_cx2cuColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_cxtwdcu(x), x$tol)
+  else
+    util_tol(util_cxtwdcu(x) %*% y, x$tol)
+}
+
 util_create <- function() {
   x <- list()
   x$X <- 0
@@ -68,8 +290,18 @@ util_rk <- function(x) {
   x$rk
 }
 
-util_uk <- function(x) {
-  x$u[,1:util_rk(x)]
+util_uk <- function(x, y) {
+  if (missing(y))
+    x$u[,1:util_rk(x)]
+  else
+    x$u[,1:util_rk(x)] %*% y
+}
+
+util_ukColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_uk(x), x$tol)
+  else
+    util_tol(util_uk(x, y), x$tol)
 }
 
 util_vk <- function(x) {
@@ -90,18 +322,26 @@ util_tol <- function(x, t) {
   return(tmp)
 }
 
-util_op <- function(x) {
+util_op <- function(x, y) {
   if (x$rk > 0)
-    tcrossprod(x$u(, 1:x$rk) * x$u(, 1:x$rk))
+    out <- tcrossprod(x$u(, 1:x$rk) * x$u(, 1:x$rk))
   else  
-    matrix(0, nrow(x$X))
+    out <- matrix(0, nrow(x$X))
+  if (missing(y))
+    return(y)
+  else
+    return(out %*% y)
 }
 
-util_opp <- function(x) {
+util_opp <- function(x, y) {
   if (x$rk > 0)
-    crossprod(x$v[, 1:x:rk], x$v[, 1:x$rk])
+    out <- crossprod(x$v[, 1:x:rk], x$v[, 1:x$rk])
   else
-    matrix(0, ncol(x$X))
+    out <- matrix(0, ncol(x$X))
+  if (missing(y))
+    return(y)
+  else
+    return(out %*% y)
 }
 
 util_pinv <- function(x) {
@@ -119,12 +359,23 @@ util_pinvxp <- function(x) {
     matrix(0, nrow(x$X), ncol(x$X))
 }
 
-util_pinvxpx <- function(x) {
+util_pinvxpx <- function(x, y) {
   r <- x$rk
   if (r > 0)
-    x$v[, 1:r] %*% tcrossprod(diag(rep(1, r) / x$ds[1:r]), x$v[, 1:r])
+    out <- x$v[, 1:r] %*% tcrossprod(diag(rep(1, r) / x$ds[1:r]), x$v[, 1:r])
   else
-    rep(0, ncol(x$X))
+    out <- rep(0, ncol(x$X))
+  if (missing(y))
+    return(out)
+  else
+    return(out %*% y)
+}
+
+util_pinvxpxColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_pinvxpx(x), x$tol)
+  else
+    util_tol(util_pinvxpx(x, y), x$tol)
 }
 
 util_jb <- function(x, n) {
@@ -155,28 +406,54 @@ util_cukpinvxp(x) {
     rep(0, ncol(x$X))
 }
 
-util_cukx <- function(x) {
+util_cukx <- function(x, y) {
   r <- x$rk
   if (r > 0)
-    tcrossprod(diag(x$ds[1:r]), x$v[, 1:r])
+    out <- tcrossprod(diag(x$ds[1:r]), x$v[, 1:r])
   else
-    rep(0, ncol(x$X))
+    out <- rep(0, ncol(x$X))
+  if (missing(y))
+    return(out)
+  else
+    return(out %*% y)
 }
 
-util_xpx <- function(x) {
+util_xpx <- function(x, y) {
   r <- x$rk
   if (r > 0)
-    x$v[, 1:r] %*% tcrossprod(diag(x$ds^2), x$v[, 1:r])
+    out <- x$v[, 1:r] %*% tcrossprod(diag(x$ds^2), x$v[, 1:r])
   else
-    rep(0, ncol(x$X))
+    out <- rep(0, ncol(x$X))
+  if (missing(y))
+    return(out)
+  else
+    return(out %*% y)
 }
 
-util_xxp <- function(x) {
+util_xpxColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_xpx(x), x$tol)
+  else
+    util_tol(util_xpx(x, y), x$tol)
+}
+
+util_xxp <- function(x, y) {
   r <- x$rk
   if (r > 0)
-    x$u[, 1:r] %*% tcrossprod(diag(x$ds^2), x$u[, 1:r])
+    out <- x$u[, 1:r] %*% tcrossprod(diag(x$ds^2), x$u[, 1:r])
   else
-    rep(0, nrow(x$X))
+    out <-rep(0, nrow(x$X))
+  if (missing(y))
+    return(out)
+  else
+    return(out %*% y)
+}
+
+util_xxpColon <- functioni(x, y) {
+  if (missing(y))
+    util_tol(util_xxp(x), x$tol)
+  else
+    util_tol(util_xxp(x, y), x$tol)
 }
 
 util_xxpY <- function(x, Y) {
@@ -187,12 +464,23 @@ util_xxpY <- function(x, Y) {
     matrix(0, nrow(x$X), ncol(x$X))
 }
 
-util_pinvxxp <- function(x) {
+util_pinvxxp <- function(x, y) {
   r <- x$rk
   if (r > 0)
-    x$u[, 1:r] %*% tcrossprod(diag(x$ds^(-2)), x$u[, 1:r])
+    out <- x$u[, 1:r] %*% tcrossprod(diag(x$ds^(-2)), x$u[, 1:r])
   else
-    rep(0, nrow(x$X))
+    out <- rep(0, nrow(x$X))
+  if (missing(y))
+    return(out)
+  else
+    return(out %*% y)
+}
+
+util_pinvxxpColon <- function(x, y) {
+  if (missing(y))
+    util_tol(util_pinvxxp(x), x$tol)
+  else
+    util_tol(util_pinvxxp(x, y), x$tol)
 }
 
 util_n <- function(x) {
@@ -232,4 +520,25 @@ util_rY <- function(x, y {
     }
   }
   return(res)
+}
+
+util_jbp = functioni(x, n) {
+  r <- x$rk
+  if (r > 0)
+    x$v[, 1:r] %*% tcrossprod(diag(x$ds[1:r]^(n)), x$v[, 1:r])
+  else
+    rep(0, ncol(x$X))
+}
+
+## from spm_SpUtil
+util_X0_2_c <- function(X0, x) {
+  if (is.null(X0)) {
+    sc0 <- utilSet(utilX(x, X0))
+    if (sc0$rk)
+      c <- utiloPp(x, utilR(sc0))
+    else
+      c <- utiloPp(x)
+    
+    
+  }
 }
