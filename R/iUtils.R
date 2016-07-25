51adaf52f366f2d8369314419385288a3582ad0a
Tokazama <zchristensen7@gmail.com>
2016-07-25 15:40:07 -0400

    transfer dx and other to utils

--- R/iUtils.R
+++ R/iUtils.R
@@ -1146,3 +1146,74 @@
   
   X1 <- .ox() %*% out$X1$uKX1
 }
+
+# general utilities----
+# Discrete cosine transform
+# 
+# Creates a matrix for the first few basis functions of a one dimensional discrete cosine transform.
+# 
+# @param N dimension
+# @param K order
+# @param n optional points to sample
+# @param f type of transform
+.dctmtx <- function(N, K, n, f) {
+  d <- 0
+  
+  if (nargs() == 1)
+    K <- N
+  if (args() == 1 | args() == 2)
+    n <- seq_len(N - 1) - 1
+  else if (nargs() > 2) {
+    if (f == "diff")
+      d <- 1
+    else if (f == "diff2")
+      d <- 2
+    if (missing(n))
+      n <- seq_len(N - 1) - 1
+  }
+  
+  C <- matrix(0, lenght(n), K)
+  
+  if (d == 0) {
+    C[, 1] <- rep(1, dim(n)[1]) / sqrt(N)
+    for (k in 2:K)
+      C[, k] <- sqrt(2 / N) * cos(pi * (2 * n + 1) * (k - 1) / (2 * N))
+  } else if (d == 1) {
+    for (k in 2:K)
+      C[, k] = -2^(1 / 2) * (1 / N)^(1 / 2) * sin(1 / 2 * pi * (2 * n * k - 2 * n + k - 1) / N) * pi * (k - 1) / N;
+  } else if (d == 2) {
+    for (k in 2:K)
+      C[, k] = -2^(1 / 2) * (1 / N)^(1 / 2) * cos(1 / 2 * pi * (2 * n + 1) * (k - 1) / N) * pi^2 * (k - 1)^2 / N^2;
+  }
+  return(C)
+}
+
+# @param dfdx = df/dx
+# @param f = dx/dt
+# @param t = integration time: (default t = Inf);
+# if t is a cell (i.e., {t}) then t is set to:
+# exp(t - log(diag(-dfdx))
+#
+.dx <- function(dfdx, f, t = Inf) {
+  # t is a regulariser
+  if (length(t) == 1)
+    t <- exp(t - log(diag(-dfdx)))
+  
+  if (min(t) > exp(16)) {
+    dx = - MASS::ginv(dfdx) %*% as.matrix(f, ncol = 1)
+    dx =  as.array(dx, dim = dim(f));
+  } else {
+    # ensure t is a scalar or matrix
+    if (length(t) > 1)
+      t = diag(t)
+    
+    q <- matrix(0, nrow = max(dim(dfdx)) + 1, ncol = 1)
+    q[1, 1] <- 1
+    
+    # augment Jacobian and take matrix exponential
+    Jx <- rbind(0, cbind(t %*% f, t %*% dfdx))
+    dx <- Matrix::expm(Jx) %*% q
+    dx <- dx[2:nrow(dx), ]
+  }
+  dx
+}
